2bf987afad62c0a3af7b8e5d33beb61b86036041
Chris Mair <chrismair@earthlink.net>
2009-02-06 12:46:04 +0000

    Add braces rules
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@34 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/test/groovy/org/codenarc/rule/braces/ForStatementBracesRuleTest.groovy
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.braces
+
+import org.codenarc.rule.AbstractRuleTest
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for ForStatementBracesRule
+ *
+ * @author Chris Mair
+ * @version $Revision: 24 $ - $Date: 2009-01-31 07:47:09 -0500 (Sat, 31 Jan 2009) $
+ */
+class ForStatementBracesRuleTest extends AbstractRuleTest {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'ForStatementBraces'
+    }
+
+    void testApplyTo_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                def myMethod() {
+                    for (int i=0; i < 10; i++) println '23'
+                    println 'ok'
+                    for (int j=0; j < 10; j++)
+                        println 'loop'
+                }
+            }
+        '''
+        assertTwoViolations(SOURCE, 4, 'for (int i=0; i < 10; i++)', 6, 'for (int j=0; j < 10; j++)')
+    }
+
+    void testApplyTo_Violation_ForStatementWithCommentOnly() {
+        final SOURCE = '''
+            for (int i=0; i < 10; i++) {
+                // TODO Should do something here
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testApplyTo_NoViolations() {
+        final SOURCE = '''class MyClass {
+                def myMethod() {
+                    for (int j=0; j < 10; j++) {
+                        println "ready"
+                    }
+                    for (int j=0; j < 10; j++) { println '23' }
+                }
+            }'''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        return new ForStatementBracesRule()
+    }
+
+}
