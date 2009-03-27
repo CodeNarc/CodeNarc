d9763c723307a71f921639a93162c16a2ee0bad5
Chris Mair <chrismair@earthlink.net>
2009-03-27 21:56:57 +0000

    ThrowThrowableRule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@99 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/exceptions/CommonThrowAstVisitor.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.exceptions
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codehaus.groovy.ast.stmt.ThrowStatement
+import org.codehaus.groovy.ast.expr.ConstructorCallExpression
+
+/**
+ * AstVisitor implementation that checks for throwing one of the exception type names specified in the constructor
+ * <p/>
+ * This is an internal class and its API is subject to change.
+ *
+ * @author Chris Mair
+ * @version $Revision: 89 $ - $Date: 2009-03-17 20:39:03 -0400 (Tue, 17 Mar 2009) $
+ */
+class CommonThrowAstVisitor extends AbstractAstVisitor  {
+    private List exceptionTypeNames
+
+    /**
+     * Construct a new instance, specifying the exception type names
+     * @param exceptionTypeNames - the names of the exception class; typically including class name
+     *      with and without package
+     */
+    CommonThrowAstVisitor(List exceptionTypeNames) {
+        this.exceptionTypeNames = exceptionTypeNames
+    }
+
+    void visitThrowStatement(ThrowStatement throwStatement) {
+        def throwExpression = throwStatement.expression
+        if (throwExpression instanceof ConstructorCallExpression &&
+                throwExpression.type.name in exceptionTypeNames) {
+            addViolation(throwStatement)
+        }
+        super.visitThrowStatement(throwStatement)
+    }
+
+}
---
+++ src/test/groovy/org/codenarc/rule/exceptions/ThrowThrowableRuleTest.groovy
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.exceptions
+
+import org.codenarc.rule.AbstractRuleTest
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for ThrowExceptionFromFinallyBlockRule
+ *
+ * @author Chris Mair
+ * @version $Revision: 24 $ - $Date: 2009-01-31 07:47:09 -0500 (Sat, 31 Jan 2009) $
+ */
+class ThrowThrowableRuleTest extends AbstractRuleTest {
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'ThrowThrowable'
+    }
+
+    void testApplyTo_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                void myMethod() {
+                    throw new Throwable()
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, 'throw new Throwable()')
+    }
+
+    void testApplyTo_Violation_FullPackageName() {
+        final SOURCE = '''
+            class MyClass {
+                void myMethod() {
+                    if (error) {
+                        throw new java.lang.Throwable('something bad')
+                    }
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 5, "throw new java.lang.Throwable('something bad')")
+    }
+
+    void testApplyTo_NoViolation() {
+        final SOURCE = '''class MyClass {
+                def myMethod() {
+                    try {
+                        throw new Exception()      // ok
+                    } finally {
+                        println 'ok'
+                    }
+                    throw new Exception()          // ok
+                }
+            }'''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        return new ThrowThrowableRule()
+    }
+
+}
