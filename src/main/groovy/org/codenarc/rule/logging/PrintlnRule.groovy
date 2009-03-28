fbc11654590b52be5a79f40629cec567f99b7ce8
Chris Mair <chrismair@earthlink.net>
2009-03-28 17:35:00 +0000

    PrintlnRule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@102 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/logging/PrintlnRule.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.logging
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codehaus.groovy.ast.expr.ConstantExpression
+
+/**
+ * Rule that checks for calls to <code>this.print()</code>, <code>this.println()</code>
+ * or <code>this.printf()</code>.
+ *
+ * @author Chris Mair
+ * @version $Revision: 98 $ - $Date: 2009-03-26 22:00:27 -0400 (Thu, 26 Mar 2009) $
+ */
+class PrintlnRule extends AbstractAstVisitorRule {
+    String name = 'Println'
+    int priority = 2
+    Class astVisitorClass = PrintlnAstVisitor
+}
+
+class PrintlnAstVisitor extends AbstractAstVisitor  {
+
+    void visitMethodCallExpression(MethodCallExpression methodCall) {
+        println methodCall
+        def isMatch =
+            AstUtil.isMethodCall(methodCall, 'this', 'println', 0) ||
+            AstUtil.isMethodCall(methodCall, 'this', 'println', 1) ||
+            AstUtil.isMethodCall(methodCall, 'this', 'print', 1) ||
+            (AstUtil.isMethodCall(methodCall, 'this', 'printf') && AstUtil.getMethodArguments(methodCall).size() > 1)
+
+        if (isMatch) {
+            addViolation(methodCall)
+        }
+        super.visitMethodCallExpression(methodCall)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/logging/PrintlnRuleTest.groovy
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.logging
+
+import org.codenarc.rule.AbstractRuleTest
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for PrintlnRule
+ *
+ * @author Chris Mair
+ * @version $Revision: 98 $ - $Date: 2009-03-26 22:00:27 -0400 (Thu, 26 Mar 2009) $
+ */
+class PrintlnRuleTest extends AbstractRuleTest {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'Println'
+    }
+
+    void testApplyTo_Println_NoArgs() {
+        final SOURCE = '''
+            println()
+        '''
+        assertSingleViolation(SOURCE, 2, 'println')
+    }
+
+    void testApplyTo_Println_OneArg() {
+        final SOURCE = '''
+            println "message"
+        '''
+        assertSingleViolation(SOURCE, 2, 'println "message"')
+    }
+
+    void testApplyTo_Println_ExplicitThis() {
+        final SOURCE = '''
+            this.println "message"
+        '''
+        assertSingleViolation(SOURCE, 2, 'this.println "message"')
+    }
+
+    void testApplyTo_Print_OneArg() {
+        final SOURCE = '''
+            print("message")
+        '''
+        assertSingleViolation(SOURCE, 2, 'print("message")')
+    }
+
+    void testApplyTo_Printf_TwoArgs() {
+        final SOURCE = '''
+            printf "%d", 99
+        '''
+        assertSingleViolation(SOURCE, 2, 'printf "%d", 99')
+    }
+
+    void testApplyTo_Printf_ThreeArgs() {
+        final SOURCE = '''
+            printf "%d, %d", 23, 34
+        '''
+        assertSingleViolation(SOURCE, 2, 'printf "%d, %d", 23, 34')
+    }
+
+    void testApplyTo_Printf_FourArgs() {
+        final SOURCE = '''
+            printf "%d, %d", 23, 34, 45
+        '''
+        assertSingleViolation(SOURCE, 2, 'printf "%d, %d", 23, 34, 45')
+    }
+
+    void testApplyTo_PrintlnButNotThis() {
+        final SOURCE = '''
+            void testSomething() {
+                System.out.println "123"
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        return new PrintlnRule()
+    }
+}
