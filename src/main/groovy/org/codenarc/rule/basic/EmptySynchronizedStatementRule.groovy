c96d5ee59b4cb8cd1e6ee23d8989f4ac0b6b4ee4
Chris Mair <chrismair@earthlink.net>
2009-04-05 00:54:58 +0000

    EmptySynchronizdStatementRule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@119 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/EmptySynchronizedStatementRule.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codehaus.groovy.ast.stmt.CatchStatement
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.stmt.SynchronizedStatement
+
+/**
+ * Rule that checks for empty synchronized statement
+ *
+ * @author Chris Mair
+ * @version $Revision: 74 $ - $Date: 2009-03-05 22:12:08 -0500 (Thu, 05 Mar 2009) $
+ */
+class EmptySynchronizedStatementRule extends AbstractAstVisitorRule {
+    String name = 'EmptySynchronizedStatement'
+    int priority = 2
+    Class astVisitorClass = EmptySynchronizedStatementAstVisitor
+}
+
+class EmptySynchronizedStatementAstVisitor extends AbstractAstVisitor  {
+    void visitSynchronizedStatement(SynchronizedStatement synchronizedStatement) {
+        if (AstUtil.isEmptyBlock(synchronizedStatement.code)) {
+            addViolation(synchronizedStatement)
+        }
+        super.visitSynchronizedStatement(synchronizedStatement)
+    }
+
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/EmptySynchronizedStatementRuleTest.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTest
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for EmptySynchronizedStatementRule
+ *
+ * @author Chris Mair
+ * @version $Revision: 24 $ - $Date: 2009-01-31 07:47:09 -0500 (Sat, 31 Jan 2009) $
+ */
+class EmptySynchronizedStatementRuleTest extends AbstractRuleTest {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'EmptySynchronizedStatement'
+    }
+
+    void testApplyTo_EmptySynchronized() {
+        final SOURCE = '''
+            class MyClass {
+                def myMethod() {
+                    synchronized(lock) {
+                    }
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, 'synchronized(lock) {')
+    }
+
+    void testApplyTo_Violation_SynchronizedContainsComment() {
+        final SOURCE = '''
+            synchronized(lock) {
+                // TODO Should do something here
+            }
+        '''
+        assertSingleViolation(SOURCE, 2, 'synchronized(lock) {')
+    }
+
+    void testApplyTo_NonEmptySynchronized() {
+        final SOURCE = '''
+            class MyClass {
+                def myMethod() {
+                    synchronized(lock) {
+                        println "bad stuff happened"
+                }
+            }'''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        return new EmptySynchronizedStatementRule()
+    }
+
+}
