f91e955c26123f1dc0ad886331d2b38337055700
Chris Mair <chrismair@earthlink.net>
2010-05-05 00:57:06 +0000

    ThreadLocalNotStaticFinalRule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@340 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/concurrency/ThreadLocalNotStaticFinalRule.groovy
@@ -0,0 +1,36 @@
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.rule.AbstractAstVisitor
+import org.codehaus.groovy.ast.FieldNode
+import org.codehaus.groovy.ast.ClassHelper
+import java.lang.reflect.Modifier
+
+/**
+ * ThreadLocal fields should be static and final. In the most common case a java.lang.ThreadLocal 
+ * instance associates state with a thread. A non-static non-final java.lang.ThreadLocal field
+ * associates state with an instance-thread combination. This is seldom necessary and often a
+ * bug which can cause memory leaks and possibly incorrect behavior.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 335 $ - $Date: 2010-05-01 02:09:20 +0200 (Sat, 01 May 2010) $
+ */
+class ThreadLocalNotStaticFinalRule extends AbstractAstVisitorRule {
+
+     String name = 'ThreadLocalNotStaticFinal'
+     int priority = 2
+     Class astVisitorClass = ThreadLocalNotStaticFinalAstVisitor
+}
+
+class ThreadLocalNotStaticFinalAstVisitor extends AbstractAstVisitor {
+
+    def void visitField(FieldNode node) {
+        if (node?.type?.name == "ThreadLocal") {
+            if ((node.modifiers & Modifier.STATIC) != Modifier.STATIC ||
+                    (node.modifiers & Modifier.FINAL) != Modifier.FINAL) {
+                addViolation(node)
+            }
+        }
+        super.visitField(node);
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/concurrency/ThreadLocalNotStaticFinalRuleTest.groovy
@@ -0,0 +1,69 @@
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for ThreadLocalNotStaticFinalRule.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class ThreadLocalNotStaticFinalRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'ThreadLocalNotStaticFinal'
+    }
+
+    void testApplyTo_Violation_StaticOrFinalButNotBoth() {
+        final SOURCE = '''
+            class ThreadLocalNotStaticFinalClass1 {
+                private static ThreadLocal local1 = new ThreadLocal()
+                private final ThreadLocal local2 = new ThreadLocal()
+
+            }
+        '''
+        assertTwoViolations(SOURCE,
+                3, 'private static ThreadLocal local1 = new ThreadLocal()',
+                4, 'private final ThreadLocal local2 = new ThreadLocal()')
+    }
+
+    void testApplyTo_Violation_NotFinalOrStatic() {
+        final SOURCE = '''
+            class ThreadLocalNotStaticFinalClass1 {
+                ThreadLocal local1 = new ThreadLocal()
+                protected ThreadLocal local2 = new ThreadLocal()
+            }
+        '''
+        assertTwoViolations(SOURCE,
+                3, 'ThreadLocal local1 = new ThreadLocal()',
+                4, 'protected ThreadLocal local2 = new ThreadLocal()')
+    }
+
+    void testApplyTo_NoViolations() {
+        final SOURCE = '''
+            class ThreadLocalNotStaticFinalClass3 {
+                private static final ThreadLocal local1 = new ThreadLocal()
+                private static final ThreadLocal local2 = new ThreadLocal()
+            }'''
+        assertNoViolations(SOURCE)
+    }
+
+    void testApplyTo_NoViolationsInnerClass() {
+        final SOURCE = '''
+            class ThreadLocalNotStaticFinalClass4 {
+                static class ThreadLocalNotStaticFinalInnerClass4 {
+                    private static final ThreadLocal local1 = new ThreadLocal()
+                    private static final ThreadLocal local2 = new ThreadLocal()
+                }
+            }'''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        return new ThreadLocalNotStaticFinalRule()
+    }
+
+}
+
