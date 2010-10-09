2ab25937421e43576d3969c391cb44354edb1837
Chris Mair <chrismair@earthlink.net>
2010-10-09 01:02:14 +0000

    DuplicateSwitchStatementRule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@389 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/DuplicateSwitchStatementRule.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.stmt.SwitchStatement
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.stmt.CaseStatement
+
+/**
+ * Rule that checks for duplicate case statements in a switch block, such as two
+ * equal integers or strings.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision$ - $Date$
+ */
+class DuplicateSwitchStatementRule extends AbstractAstVisitorRule {
+    String name = 'DuplicateSwitchStatement'
+    int priority = 2
+    Class astVisitorClass = DuplicateSwitchStatementAstVisitor
+}
+
+class DuplicateSwitchStatementAstVisitor extends AbstractAstVisitor {
+
+    void visitSwitch(SwitchStatement statement) {
+        def allElements = statement?.caseStatements?.findAll { it?.expression instanceof ConstantExpression }
+        allElements.inject([]) { list, CaseStatement item  ->
+            if (list.contains(item?.expression?.value)) {
+                addViolation(item, 'Duplicate case statements found in switch')
+            }
+            else {
+                list.add item.expression?.value
+            }
+            list
+        }
+    super.visitSwitch(statement)
+  }
+
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/DuplicateSwitchStatementRuleTest.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for DuplicateSwitchStatementRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision$ - $Date$
+ */
+class DuplicateSwitchStatementRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "DuplicateSwitchStatement"
+    }
+
+    void testApplyTo_NoViolations() {
+        final SOURCE = '''
+            def value = 2
+            switch( value ) {
+              case 1:
+                break;
+              case 2:
+                break;
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testApplyTo_SingleViolationInteger() {
+        final SOURCE = '''
+            switch( 0 ) {
+                case 1:
+                  break;
+                case 2:
+                  break;
+                case 2:
+                  break;
+            }
+        '''
+        assertSingleViolation(SOURCE, 7, 'case 2:')
+    }
+
+    void testApplyTo_SingleViolationString() {
+        final SOURCE = '''
+            switch( "test" ) {
+                case "$a":
+                  break;
+                case "$a":
+                  break;
+                case "ab":
+                  break;
+                case "ab":
+                  break;
+                case "abc":
+                  break;
+            }
+        '''
+        assertSingleViolation(SOURCE, 9, 'case "ab":')
+    }
+
+    protected Rule createRule() {
+        return new DuplicateSwitchStatementRule()
+    }
+
+}
