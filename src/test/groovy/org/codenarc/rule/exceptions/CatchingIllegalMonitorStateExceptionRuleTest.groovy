9f1c1203daa18c11724b96845779a971e3720d1d
Hamlet D'Arcy <hamletdrc@gmail.com>
2010-10-27 18:54:23 +0000

    Catching IllegalMonitorStateException Rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@397 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/test/groovy/org/codenarc/rule/exceptions/CatchingIllegalMonitorStateExceptionRuleTest.groovy
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.exceptions
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for CatchingIllegalMonitorStateExceptionRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class CatchingIllegalMonitorStateExceptionRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "CatchingIllegalMonitorStateException"
+    }
+
+    void testApplyTo_Violation() {
+        final SOURCE = '''
+                try {
+                    doSomething()
+                }
+                catch(IllegalMonitorStateException t) {
+                }
+        '''
+        assertSingleViolation(SOURCE, 5, 'catch(IllegalMonitorStateException t) {')
+    }
+
+    void testApplyTo_Violation_FullPackageName() {
+        final SOURCE = 'try {  } catch(java.lang.IllegalMonitorStateException t) { }'
+        assertSingleViolation(SOURCE, 1, 'catch(java.lang.IllegalMonitorStateException t) {')
+    }
+
+    void testApplyTo_NoViolations() {
+        final SOURCE = '''
+                def myMethod() {
+                    try {
+                    } catch(Exception t) { }
+                }
+            '''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        return new CatchingIllegalMonitorStateExceptionRule()
+    }
+
+}
