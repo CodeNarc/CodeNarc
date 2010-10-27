ee00415eed13b6c1f23af19f46c83de40a597ca7
Hamlet D'Arcy <hamletdrc@gmail.com>
2010-10-27 19:05:15 +0000

    use of Thread.notify() method instead of notifyAll()
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@401 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/concurrency/UseOfNotifyMethodRule.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codenarc.util.AstUtil
+
+/**
+ * This code calls notify() rather than notifyAll(). Java monitors are often used for multiple conditions. Calling notify() only wakes up one thread, meaning that the thread woken up might not be the one waiting for the condition that the caller just satisfied.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class UseOfNotifyMethodRule extends AbstractAstVisitorRule {
+    String name = 'UseOfNotifyMethod'
+    int priority = 2
+    Class astVisitorClass = UseOfNotifyMethodAstVisitor
+}
+
+class UseOfNotifyMethodAstVisitor extends AbstractAstVisitor {
+
+    def void visitMethodCallExpression(MethodCallExpression call) {
+        if (AstUtil.isMethodNamed(call, "notify")) {
+            if (call.arguments.properties['expressions']?.size() == 0) {
+                addViolation call
+            }
+        }
+    }
+
+
+}
---
+++ src/test/groovy/org/codenarc/rule/concurrency/UseOfNotifyMethodRuleTest.groovy
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for UseOfNotifyMethodRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class UseOfNotifyMethodRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "UseOfNotifyMethod"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+        	notifyAll()
+            notify(foo)
+            notify foo
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testThisNotify() {
+        final SOURCE = '''
+            this.notify()
+        '''
+        assertSingleViolation(SOURCE, 2, 'this.notify()')
+    }
+
+    void testNotify() {
+        final SOURCE = '''
+            notify()
+        '''
+        assertSingleViolation(SOURCE, 2, 'notify()')
+    }
+
+    void testOther() {
+        final SOURCE = '''
+            other.notify()
+        '''
+        assertSingleViolation(SOURCE, 2, 'other.notify()')
+    }
+
+    protected Rule createRule() {
+        return new UseOfNotifyMethodRule()
+    }
+}
