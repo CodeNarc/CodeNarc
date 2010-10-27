e506f94eb0d9b5974ab83c610dab037854305e8e
Hamlet D'Arcy <hamletdrc@gmail.com>
2010-10-27 19:02:44 +0000

    Synchronized on getClass() rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@400 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/test/groovy/org/codenarc/rule/concurrency/SynchronizedOnGetClassRuleTest.groovy
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for SynchronizedOnGetClassRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class SynchronizedOnGetClassRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "SynchronizedOnGetClass"
+    }
+
+
+    void testBadSynchronization() {
+        final SOURCE = '''
+                class MyClass {
+                    def Method1() {
+                        synchronized(getClass()) { ; }
+                    }
+                }
+            '''
+        assertSingleViolation(SOURCE, 4, 'synchronized(getClass()) { ; }')
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+                class MyClass {
+
+                    def Method1() {
+                        synchronized(getFoo()) { ; }
+                    }
+                    def Method2() {
+                        synchronized(MyClass) { ; }
+                    }
+                    def Method3() {
+                        synchronized(String.class) { ; }
+                    }
+                }'''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        return new SynchronizedOnGetClassRule()
+    }
+}
