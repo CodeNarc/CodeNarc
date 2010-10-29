25c7d8fb9c7b7e549615a7be41f51c9522712821
Hamlet D'Arcy <hamletdrc@gmail.com>
2010-10-29 18:58:23 +0000

    Too Many Methods Rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@414 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/size/TooManyMethodsRule.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.size
+
+import org.codehaus.groovy.ast.ClassNode
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * A class with too many methods is probably a good suspect for refactoring, in order to reduce its complexity and find
+ * a way to have more fine grained objects.
+ *
+ * @author Tomasz Bujok
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class TooManyMethodsRule extends AbstractAstVisitorRule {
+  String name = 'TooManyMethods'
+  int priority = 2
+  Class astVisitorClass = TooManyMethodsAstVisitor
+  int maxMethods = 50
+}
+
+class TooManyMethodsAstVisitor extends AbstractAstVisitor {
+  def void visitClass(ClassNode node) {
+    super.visitClass(node);
+    if (node.methods?.size() > rule.maxMethods) {
+      addViolation node
+    }
+  }
+
+}
