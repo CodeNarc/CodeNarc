6c11c0b4392f3ef5cfb8a1f4197d846baf5e2797
Chris Mair <chrismair@earthlink.net>
2010-11-06 21:04:46 +0000

    Add Apache License 2.0 comment where missing. Add new custom ApacheLicenseRequired rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@429 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

--- src/test/groovy/org/codenarc/SiteDocumentationTest.groovy
+++ src/test/groovy/org/codenarc/SiteDocumentationTest.groovy
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codenarc
 
 /**
  * Tests that the "site" documentation is up to date.
  *
  * @author Hamlet D'Arcy
+ * @version $Revision: 292 $ - $Date: 2010-01-16 22:09:23 -0500 (Sat, 16 Jan 2010) $
  */
 class SiteDocumentationTest extends GroovyTestCase {
 
