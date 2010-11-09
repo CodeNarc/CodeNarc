ff23a5e7bbbdbc69aa839440f9675fb308ef75ef
Chris Mair <chrismair@earthlink.net>
2010-11-09 00:06:04 +0000

    Move general tests into org.codenarc.test
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@440 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

--- src/test/groovy/org/codenarc/test/SiteDocumentationTest.groovy
+++ src/test/groovy/org/codenarc/test/SiteDocumentationTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.codenarc
+package org.codenarc.test
 
 /**
  * Tests that the "site" documentation is up to date.
