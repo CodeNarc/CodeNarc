4f21b4bcd5531ff645ca2e02b67fd43bb6bb7765
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-11 19:04:06 +0000

    final class with protected member rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@510 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/design/FinalClassWithProtectedMemberRule.groovy
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import java.lang.reflect.Modifier
+import org.codehaus.groovy.ast.FieldNode
+import org.codehaus.groovy.ast.MethodNode
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * This rule finds classes marked final that contain protected methods and fields. If a class is final then it may not be subclassed,
+ * and there is therefore no point in having a member with protected visibility. Either the class should not be final or
+ * the member should be private or protected.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class FinalClassWithProtectedMemberRule extends AbstractAstVisitorRule {
+    String name = 'FinalClassWithProtectedMember'
+    int priority = 2
+    Class astVisitorClass = FinalClassWithProtectedMemberAstVisitor
+}
+
+class FinalClassWithProtectedMemberAstVisitor extends AbstractAstVisitor {
+    @Override
+    protected void visitConstructorOrMethodEx(MethodNode node, boolean isConstructor) {
+        if (node.isProtected()) {
+            if (Modifier.isFinal(node.declaringClass.modifiers)) {
+                addViolation node, "The method $node.name has protected visibility but the enclosing class $node.declaringClass.name is marked final"
+            }
+        }
+        super.visitConstructorOrMethodEx(node, isConstructor)
+    }
+
+    @Override
+    void visitFieldEx(FieldNode node) {
+        if (isProtected(node)) {
+            if (Modifier.isFinal(node.declaringClass.modifiers)) {
+                addViolation node, "The field $node.name has protected visibility but the enclosing class $node.declaringClass.name is marked final"
+            }
+        }
+        super.visitFieldEx(node)
+    }
+
+
+    private static boolean isProtected(node) {
+        Modifier.isProtected(node.modifiers)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/design/FinalClassWithProtectedMemberRuleTest.groovy
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for FinalClassWithProtectedMemberRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class FinalClassWithProtectedMemberRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "FinalClassWithProtectedMember"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+        	final class MyClass1 {
+                public method1() {}
+                def method2() {}
+                @PackageScope def method3() {}
+                private def method3() {}
+            }
+        	class MyClass2 {
+                protected def method() {}
+                protected def closure = {}
+                protected String property
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testMethodFailure() {
+        final SOURCE = '''
+        	final class MyClass {
+                protected def methodName() {}
+            }
+        '''
+        assertSingleViolation(SOURCE,
+                3,
+                'protected def methodName() {}',
+                'The method methodName has protected visibility but the enclosing class MyClass is marked final')
+    }
+
+    void testFieldFailure() {
+        final SOURCE = '''
+        	final class MyClass {
+                protected def closure = {}
+            }
+        '''
+        assertSingleViolation(SOURCE,
+                3,
+                'protected def closure = {}',
+                'The field closure has protected visibility but the enclosing class MyClass is marked final')
+    }
+
+    void testPropertyFailure() {
+        final SOURCE = '''
+        	final class MyClass {
+                protected String myProperty
+            }
+        '''
+        assertSingleViolation(SOURCE,
+                3,
+                'protected String myProperty',
+                'The field myProperty has protected visibility but the enclosing class MyClass is marked final')
+    }
+
+    protected Rule createRule() {
+        new FinalClassWithProtectedMemberRule()
+    }
+}
