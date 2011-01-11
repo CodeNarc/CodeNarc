3e299410ec8cfa121d683b79e95554990b2422c6
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-11 19:43:20 +0000

    rule to catch explicitly calling garbage collection
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@511 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/ExplicitGarbageCollectionRule.groovy
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.expr.PropertyExpression
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.ast.expr.Expression
+
+/**
+ * Calls to System.gc(), Runtime.getRuntime().gc(), and System.runFinalization() are not advised. Code should have the
+ * same behavior whether the garbage collection is disabled using the option -Xdisableexplicitgc or not. Moreover,
+ * "modern" jvms do a very good job handling garbage collections. If memory usage issues unrelated to memory leaks
+ * develop within an application, it should be dealt with JVM options rather than within the code itself.
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class ExplicitGarbageCollectionRule extends AbstractAstVisitorRule {
+    String name = 'ExplicitGarbageCollection'
+    int priority = 2
+    Class astVisitorClass = ExplicitGarbageCollectionAstVisitor
+}
+
+class ExplicitGarbageCollectionAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+        if (AstUtil.isMethodCall(call, ['System'], ['gc', 'runFinalization'], 0)) {
+            addViolation call, 'Garbage collection should not be explicitly forced'
+        } else if (AstUtil.isMethodNamed(call, 'gc', 0)) {
+            if (isPropertyExpression(call.objectExpression, 'Runtime', 'runtime')) {
+                addViolation call, 'Garbage collection should not be explicitly forced'
+            } else if (AstUtil.isMethodCall(call.objectExpression, 'Runtime', 'getRuntime', 0)) {
+                addViolation call, 'Garbage collection should not be explicitly forced'
+            }
+
+        } 
+
+
+        super.visitMethodCallExpression call
+    }
+
+    private boolean isPropertyExpression(Expression expression, String object, String propertyName) {
+        if (!(expression instanceof PropertyExpression)) {
+            return false
+        }
+
+        if (expression.property instanceof ConstantExpression && expression.property.value == propertyName) {
+            if (expression.objectExpression instanceof VariableExpression && expression.objectExpression.variable == object) {
+                return true
+            }
+        }
+        false
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/ExplicitGarbageCollectionRuleTest.groovy
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for ExplicitGarbageCollectionRule
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class ExplicitGarbageCollectionRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "ExplicitGarbageCollection"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            System.gc(666)
+            System2.gc()
+            Runtime2.getRuntime().gc()
+            Runtime.getRuntime2().gc()
+            Runtime.getRuntime(666).gc()
+            Runtime.getRuntime().gc(666)
+            System.runFinalization(666)
+            System2.runFinalization()
+            Runtime.runtime.gc2()
+            Runtime.runtime.gc(666)
+            Runtime2.runtime.gc()
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testSystemGC() {
+        final SOURCE = '''
+            System.gc()
+        '''
+        assertSingleViolation(SOURCE, 2, 'System.gc()', 'Garbage collection should not be explicitly forced')
+    }
+
+    void testSystemFinalization() {
+        final SOURCE = '''
+            System.runFinalization()
+        '''
+        assertSingleViolation(SOURCE, 2, 'System.runFinalization()', 'Garbage collection should not be explicitly forced')
+    }
+
+    void testRuntimeGcMethodCall() {
+        final SOURCE = '''
+            Runtime.getRuntime().gc()
+        '''
+        assertSingleViolation(SOURCE,
+                2, 'Runtime.getRuntime().gc()', 'Garbage collection should not be explicitly forced')
+    }
+
+    void testRuntimeGcPropertyInvocation() {
+        final SOURCE = '''
+            Runtime.runtime.gc()
+        '''
+        assertSingleViolation(SOURCE,
+                2, 'Runtime.runtime.gc()', 'Garbage collection should not be explicitly forced')
+    }
+
+    protected Rule createRule() {
+        new ExplicitGarbageCollectionRule()
+    }
+}
