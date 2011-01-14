9e93f13e5b8fe04361fc59c501aaa57df30231a9
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-14 19:51:40 +0000

    Rule to catch multiple loggers defined in single class.
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@521 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/logging/MultipleLoggersRule.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.logging
+
+import org.codehaus.groovy.ast.ClassNode
+import org.codehaus.groovy.ast.FieldNode
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * This rule catches classes that have more than one logger object defined. Typically, a class has zero or one logger objects. 
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class MultipleLoggersRule extends AbstractAstVisitorRule {
+    String name = 'MultipleLoggers'
+    int priority = 2
+    Class astVisitorClass = MultipleLoggersAstVisitor
+}
+
+class MultipleLoggersAstVisitor extends AbstractAstVisitor {
+
+    Map<ClassNode, List<String>> classNodeToFieldNames = [:]
+
+    @Override
+    void visitFieldEx(FieldNode fieldNode) {
+        if (LogUtils.isMatchingLoggerDefinition(fieldNode.getInitialExpression())) {
+
+            List<String> logFields = classNodeToFieldNames[fieldNode.declaringClass]
+            if (logFields) {
+                logFields.add(fieldNode.name)
+                addViolation(fieldNode, "The class $fieldNode.declaringClass.name defines multiple loggers: " + logFields.join(', '))
+            } else {
+                classNodeToFieldNames[fieldNode.declaringClass] = [fieldNode.name]
+            }
+        }
+        super.visitFieldEx(fieldNode)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/logging/MultipleLoggersRuleTest.groovy
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.logging
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for MultipleLoggersRule
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class MultipleLoggersRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "MultipleLoggers"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            class MyClass {
+                private static final LOG = Logger.getLogger(MyClass)
+                private static final LOG2 = '' // not a logger
+            }
+            class MyOtherClass {
+                private static final LOG3 = Logger.getLogger(MyClass)
+                class MyInnerClass {
+                    private static final LOG4 = Logger.getLogger(MyClass)
+                }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testSingleViolation() {
+        final SOURCE = '''
+            class MyClass {
+                def LOG = Logger.getLogger(MyClass)
+                def logger = Logger.getLogger(MyClass)
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, 'def logger = Logger.getLogger(MyClass)', 'The class MyClass defines multiple loggers: LOG, logger')
+    }
+
+    void testInnerClass() {
+        final SOURCE = '''
+            class MyClass {
+                class MyInnerClass {
+                    def LOG = Logger.getLogger(MyClass)
+                    def logger = Logger.getLogger(MyClass)
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 5, 'def logger = Logger.getLogger(MyClass)', 'The class MyClass$MyInnerClass defines multiple loggers: LOG, logger')
+    }
+
+    protected Rule createRule() {
+        new MultipleLoggersRule()
+    }
+}
