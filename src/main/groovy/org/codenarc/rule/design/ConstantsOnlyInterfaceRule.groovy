cee67d8ed356551459f01e72d512224c28685885
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-16 08:28:01 +0000

    Rule for ConstantsOnlyInterface
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@528 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/design/ConstantsOnlyInterfaceRule.groovy
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.ClassNode
+
+/**
+ * An interface should be used only to model a behaviour of a class: using an interface as a container of constants is a poor usage pattern.
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class ConstantsOnlyInterfaceRule extends AbstractAstVisitorRule {
+    String name = 'ConstantsOnlyInterface'
+    int priority = 2
+    Class astVisitorClass = ConstantsOnlyInterfaceAstVisitor
+}
+
+class ConstantsOnlyInterfaceAstVisitor extends AbstractAstVisitor {
+    @Override
+    protected void visitClassEx(ClassNode node) {
+
+        if (node.isInterface() && node.fields && !node.methods) {
+            addViolation(node, "The interface $node.name has only fields and no methods defined")
+        }
+        super.visitClassEx(node)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/design/ConstantsOnlyInterfaceRuleTest.groovy
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for ConstantsOnlyInterfaceRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class ConstantsOnlyInterfaceRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "ConstantsOnlyInterface"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            public interface Interface1 {
+                public static final int CONSTANT_1 = 0
+                public static final String CONSTANT_2 = "1"
+                void method();
+            }
+            public interface Interface2 {
+                // don't know why you'd want to do this, but it is legal
+            }
+            public interface Interface3 {
+                void method();
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testSuccessInScript() {
+        final SOURCE = '''
+            int CONSTANT_1 = 0
+            String CONSTANT_2 = "1"
+            println CONSTANT_1
+        '''
+        assertNoViolations(SOURCE)
+    }
+    
+    void testViolation() {
+        final SOURCE = '''
+            public interface ConstantInterface {
+                public static final int CONSTANT_1 = 0
+                public static final String CONSTANT_2 = "1"
+            }
+        '''
+        assertSingleViolation(SOURCE,
+                2,  'public interface ConstantInterface',
+                'The interface ConstantInterface has only fields and no methods defined')
+    }
+
+    protected Rule createRule() {
+        new ConstantsOnlyInterfaceRule()
+    }
+}
