ad8d3b93d0e419486b2ba1432644028f741d8fa2
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-20 17:30:45 +0000

    avoid thread group rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@551 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/concurrency/AvoidThreadGroupRule.groovy
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codehaus.groovy.ast.expr.ConstructorCallExpression
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * Avoid using ThreadGroup; although it is intended to be used in a threaded environment it contains methods that are not thread safe.
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class AvoidThreadGroupRule extends AbstractAstVisitorRule {
+    String name = 'AvoidThreadGroup'
+    int priority = 2
+    Class astVisitorClass = AvoidThreadGroupAstVisitor
+}
+
+class AvoidThreadGroupAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitConstructorCallExpression(ConstructorCallExpression call) {
+
+        if (isConstructorNamed(call, ThreadGroup)) {
+            addViolation(call, 'Avoid using java.lang.ThreadGroup; it is unsafe')
+        }
+        super.visitConstructorCallExpression(call)
+    }
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+
+        if (AstUtil.isMethodNamed(call, 'getThreadGroup', 0)) {
+            addViolation(call, 'Avoid using java.lang.ThreadGroup; it is unsafe')
+        }
+        super.visitMethodCallExpression(call)
+    }
+
+
+    private static boolean isConstructorNamed(ConstructorCallExpression call, Class clazz) {
+        call.type.name == clazz.name || call.type.name == clazz.simpleName
+    }
+
+}
---
+++ src/test/groovy/org/codenarc/rule/concurrency/AvoidThreadGroupRuleTest.groovy
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for AvoidThreadGroupRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class AvoidThreadGroupRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "AvoidThreadGroup"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+        	getThreadGroup('...')    // a parameter means it must not be a getter.  
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testConstructors() {
+        final SOURCE = '''
+            new ThreadGroup("...")
+        '''
+        assertSingleViolation(SOURCE, 2, 'new ThreadGroup("...")', 'Avoid using java.lang.ThreadGroup; it is unsafe')
+    }
+
+    void testConstructorsFullyQualified() {
+        final SOURCE = '''
+            new java.lang.ThreadGroup("...")
+        '''
+        assertSingleViolation(SOURCE, 2, 'new java.lang.ThreadGroup("...")', 'Avoid using java.lang.ThreadGroup; it is unsafe')
+    }
+
+    void testConstructors2() {
+        final SOURCE = '''
+            new ThreadGroup(tg, "my thread group")
+        '''
+        assertSingleViolation(SOURCE, 2, 'new ThreadGroup(tg, "my thread group")', 'Avoid using java.lang.ThreadGroup; it is unsafe')
+    }
+
+    void testFromCurrentThread() {
+        final SOURCE = '''
+            Thread.currentThread().getThreadGroup()
+        '''
+        assertSingleViolation(SOURCE, 2, 'Thread.currentThread().getThreadGroup()', 'Avoid using java.lang.ThreadGroup; it is unsafe')
+    }
+
+    void testFromSecurityManager() {
+        final SOURCE = '''
+            System.getSecurityManager().getThreadGroup()
+        '''
+        assertSingleViolation(SOURCE, 2, 'System.getSecurityManager().getThreadGroup()', 'Avoid using java.lang.ThreadGroup; it is unsafe')
+    }
+
+    protected Rule createRule() {
+        new AvoidThreadGroupRule()
+    }
+}
