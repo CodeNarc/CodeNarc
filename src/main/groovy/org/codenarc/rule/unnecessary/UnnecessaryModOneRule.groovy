e6f9ad8ba412f4052f9629804a26a49cdeeb2e0e
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-25 20:37:38 +0000

    unnecessary mod 1 rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@567 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/unnecessary/UnnecessaryModOneRule.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.expr.BinaryExpression
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.Expression
+
+/**
+ * Any expression mod 1 (exp % 1) is guaranteed to always return zero. This code is probably an error, and should be either (exp & 1) or (exp % 2).
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class UnnecessaryModOneRule extends AbstractAstVisitorRule {
+    String name = 'UnnecessaryModOne'
+    int priority = 2
+    Class astVisitorClass = UnnecessaryModOneAstVisitor
+}
+
+class UnnecessaryModOneAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitBinaryExpression(BinaryExpression expression) {
+
+        if (AstUtil.isBinaryExpressionType(expression, '%')) {
+            Expression rhs = expression.rightExpression
+            if (rhs instanceof ConstantExpression && rhs.value == 1) {
+                Expression lhs = expression.leftExpression
+                addViolation(expression, "$expression.text is guaranteed to be zero. Did you mean ($lhs.text & 1) or ($lhs.text % 2)")
+            }
+        }
+        super.visitBinaryExpression(expression)
+    }
+
+
+}
---
+++ src/test/groovy/org/codenarc/rule/unnecessary/UnnecessaryModOneRuleTest.groovy
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for UnnecessaryModOneRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class UnnecessaryModOneRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "UnnecessaryModOne"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            if (exp & 1) {}     // ok
+            if (exp % 2) {}     // ok
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testViolations() {
+        final SOURCE = '''
+            if (exp % 1) {}         // violation
+            if (method() % 1) {}    // violation
+        '''
+        assertTwoViolations(SOURCE,
+                2, '(exp % 1)', "(exp % 1) is guaranteed to be zero. Did you mean (exp & 1) or (exp % 2)",
+                3, '(method() % 1)', "(this.method() % 1) is guaranteed to be zero. Did you mean (this.method() & 1) or (this.method() % 2)")
+    }
+
+    protected Rule createRule() {
+        new UnnecessaryModOneRule()
+    }
+}
