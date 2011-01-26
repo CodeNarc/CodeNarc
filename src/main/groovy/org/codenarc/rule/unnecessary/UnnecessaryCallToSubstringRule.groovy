87098681222e10f4d6ed3cab43f82623b4237996
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-26 19:40:00 +0000

    Unnecessary call to Substring rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@574 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/unnecessary/UnnecessaryCallToSubstringRule.groovy
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * Calling String.substring(0) always returns the original string. This code is meaningless.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class UnnecessaryCallToSubstringRule extends AbstractAstVisitorRule {
+    String name = 'UnnecessaryCallToSubstring'
+    int priority = 2
+    Class astVisitorClass = UnnecessaryCallToSubstringAstVisitor
+}
+
+class UnnecessaryCallToSubstringAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+
+        if (AstUtil.isMethodNamed(call, 'substring', 1)) {
+            def arg = AstUtil.getMethodArguments(call)[0]
+            if (AstUtil.isConstant(arg, 0)) {
+                addViolation(call, 'Invoking the String method substring(0) always returns the original value. Method possibly missing 2nd parameter')
+            }
+        }
+        super.visitMethodCallExpression(call)
+    }
+
+
+}
---
+++ src/test/groovy/org/codenarc/rule/unnecessary/UnnecessaryCallToSubstringRuleTest.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for UnnecessaryCallToSubstringRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class UnnecessaryCallToSubstringRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "UnnecessaryCallToSubstring"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+        	prop.substring(1)
+            prop.substring(0, 1)
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testSingleViolation() {
+        final SOURCE = '''
+            prop.substring(0)
+        '''
+        assertSingleViolation(SOURCE, 2, 'prop.substring(0)', 'Invoking the String method substring(0) always returns the original value. Method possibly missing 2nd parameter')
+    }
+
+    protected Rule createRule() {
+        new UnnecessaryCallToSubstringRule()
+    }
+}
