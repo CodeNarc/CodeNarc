16223e925d2d1ddfb258f9b335888421abf19991
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-27 20:58:51 +0000

    synchronized read object method
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@579 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/concurrency/SynchronizedReadObjectMethodRule.groovy
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import java.lang.reflect.Modifier
+import org.codehaus.groovy.ast.ClassNode
+import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.Parameter
+import org.codehaus.groovy.ast.stmt.BlockStatement
+import org.codehaus.groovy.ast.stmt.Statement
+import org.codehaus.groovy.ast.stmt.SynchronizedStatement
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * Catches Serializable classes that define a synchronized readObject method. By definition, an object created by deserialization is only reachable by one thread, and thus there is no need for readObject() to be synchronized. If the readObject() method itself is causing the object to become visible to another thread, that is an example of very dubious coding style.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class SynchronizedReadObjectMethodRule extends AbstractAstVisitorRule {
+    String name = 'SynchronizedReadObjectMethod'
+    int priority = 2
+    Class astVisitorClass = SynchronizedReadObjectMethodAstVisitor
+}
+
+    class SynchronizedReadObjectMethodAstVisitor extends AbstractAstVisitor {
+        ClassNode serializableClass = null
+        String currentClassName = null
+
+        @Override
+        protected void visitClassEx(ClassNode node) {
+
+            currentClassName = node.name
+            node.interfaces?.each {
+                if (AstUtil.classNodeImplementsType(it, Serializable)) {
+                    serializableClass = it
+                }
+            }
+            super.visitClassEx(node)
+        }
+
+
+        @Override
+        void visitMethodEx(MethodNode node) {
+
+            if (serializableClass && AstUtil.isMethodNode(node, 'readObject', 1)
+                    && Modifier.isPrivate(node.modifiers)) {
+                Parameter parm = node.parameters[0]
+                if (node?.returnType?.name == 'void' && AstUtil.classNodeImplementsType(parm.type, ObjectInputStream)) {
+
+                    if (Modifier.isSynchronized(node.modifiers)) {
+                        addViolation(node, "The Serializable class $currentClassName has a synchronized readObject method. It is normally unnecesary to synchronize within deserializable")
+                    } else if (isSynchronizedBlock(node.code)) {
+                        addViolation(node.code, "The Serializable class $currentClassName has a synchronized readObject method. It is normally unnecesary to synchronize within deserializable")
+                    }
+                }
+            }
+            super.visitMethodEx(node)
+        }
+
+        static boolean isSynchronizedBlock(Statement statement) {
+            if (!(statement instanceof BlockStatement)) {
+                return false
+            }
+            
+            if (statement.statements.size() != 1) {
+                return false
+            }
+
+            statement.statements[0] instanceof SynchronizedStatement
+        }
+
+
+    }
---
+++ src/test/groovy/org/codenarc/rule/concurrency/SynchronizedReadObjectMethodRuleTest.groovy
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for SynchronizedReadObjectMethodRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class SynchronizedReadObjectMethodRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "SynchronizedReadObjectMethod"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+          // OK, class not Serializable
+          class MyClass1 {
+              private synchronized void readObject(ObjectInputStream input) throws IOException, ClassNotFoundException { }
+          }
+
+          // OK, class not Serializable
+          class MyClass2 {
+              private void readObject(ObjectInputStream input) throws IOException, ClassNotFoundException {
+                  synchronized(lock) { }
+              }
+          }
+
+          // OK, Synchronized block is sufficiently advanced
+          class MyClass3 {
+              private void readObject(ObjectInputStream input) throws IOException, ClassNotFoundException {
+                  synchronized(lock) { }
+                  doSomething()
+              }
+          }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testSynchronizedMethod() {
+        final SOURCE = '''
+            class MyClass implements Serializable {
+
+                private synchronized void readObject(ObjectInputStream input) throws IOException, ClassNotFoundException {
+                    // violation, no need to synchronized
+                }
+            }
+      '''
+        assertSingleViolation(SOURCE, 4, 'synchronized void readObject', 'The Serializable class MyClass has a synchronized readObject method. It is normally unnecesary to synchronize within deserializable')
+    }
+
+    void testSynchronizedBlock() {
+        final SOURCE = '''
+            class MyClass implements Serializable {
+
+                private void readObject(ObjectInputStream input) throws IOException, ClassNotFoundException {
+                    synchronized(lock) {
+                        // violation, no need to synchronized
+                    }
+                }
+            }
+      '''
+        assertSingleViolation(SOURCE, 5, 'synchronized(lock)', 'The Serializable class MyClass has a synchronized readObject method. It is normally unnecesary to synchronize within deserializable')
+    }
+
+    protected Rule createRule() {
+        new SynchronizedReadObjectMethodRule()
+    }
+}
