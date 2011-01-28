0b51663d176d803d18cf1f90c9eee0741be26b14
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-01-28 19:52:28 +0000

    Unnecessary Self Assignment Rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@580 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/unnecessary/UnnecessarySelfAssignmentRule.groovy
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codehaus.groovy.ast.expr.BinaryExpression
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.expr.PropertyExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * Method contains a pointless self-assignment to a variable or property.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class UnnecessarySelfAssignmentRule extends AbstractAstVisitorRule {
+    String name = 'UnnecessarySelfAssignment'
+    int priority = 2
+    Class astVisitorClass = UnnecessarySelfAssignmentAstVisitor
+}
+
+class UnnecessarySelfAssignmentAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitBinaryExpression(BinaryExpression expression) {
+
+        if (AstUtil.isBinaryExpressionType(expression, '=')) {
+            Expression left = expression.leftExpression
+            Expression right = expression.rightExpression
+            if (left instanceof VariableExpression && right instanceof VariableExpression) {
+                if (left.name == right.name) {
+                    addViolation(expression, 'Assignment a variable to itself should be unnecessary. Remove this dead code')
+                }
+            } else if (propertyExpressionsAreEqual(left, right)) {
+                if (left.text == right.text) {
+                    addViolation(expression, 'Assignment a variable to itself should be unnecessary. Remove this dead code')
+                }
+            }
+        }
+        super.visitBinaryExpression(expression)
+    }
+
+    private static boolean propertyExpressionsAreEqual(Expression left, Expression right) {
+
+        def stack = [[left, right]] as Stack
+        while (stack) {
+            (left, right) = stack.pop()
+            if (!(left instanceof PropertyExpression) || !(right instanceof PropertyExpression)) {
+                return false
+            }
+            if (left.text != right.text) {
+                return false
+            }
+            if (left.spreadSafe != right.spreadSafe || left.safe != right.safe) {
+                return false
+            }
+            if (left.objectExpression instanceof PropertyExpression && right.objectExpression instanceof PropertyExpression) {
+                // recursive alternative
+                stack << [left.objectExpression, right.objectExpression]
+            }
+        }
+        true
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/unnecessary/UnnecessarySelfAssignmentRuleTest.groovy
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for UnnecessarySelfAssignmentRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class UnnecessarySelfAssignmentRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "UnnecessarySelfAssignment"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            x = y               // acceptable
+            a.b = a.zz          // acceptable
+            a.b = a().b         // acceptable
+            a.b.c = a?.b?.c     // acceptable
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testVariableAssignment() {
+        final SOURCE = '''
+            x = x               // violation
+        '''
+        assertSingleViolation(SOURCE, 2, 'x = x', 'Assignment a variable to itself should be unnecessary. Remove this dead code')
+    }
+
+    void testParameterAssignment() {
+        final SOURCE = '''
+            def method(y) {
+                y = y           // violation
+            }
+        '''
+        assertSingleViolation(SOURCE, 3, 'y = y', 'Assignment a variable to itself should be unnecessary. Remove this dead code')
+    }
+
+    void testPropertyAssignment() {
+        final SOURCE = '''
+            a.b = a.b       // violation
+        '''
+        assertSingleViolation(SOURCE, 2, 'a.b = a.b', 'Assignment a variable to itself should be unnecessary. Remove this dead code')
+    }
+
+    void testPropertyAssignment2() {
+        final SOURCE = '''
+            a.b.c = a.b.c       // violation
+        '''
+        assertSingleViolation(SOURCE, 2, 'a.b.c = a.b.c', 'Assignment a variable to itself should be unnecessary. Remove this dead code')
+    }
+
+    protected Rule createRule() {
+        new UnnecessarySelfAssignmentRule()
+    }
+}
