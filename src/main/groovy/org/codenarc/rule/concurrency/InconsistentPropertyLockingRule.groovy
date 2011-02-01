ab082d8efa2128939cded22cf8718bb7a81ce346
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-01 13:03:08 +0000

    inconsistent property locking rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@583 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/concurrency/InconsistentPropertyLockingRule.groovy
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codehaus.groovy.ast.MethodNode
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * Class contains similarly-named get and set methods where one method of the pair is marked either @WithReadLock or @WithWriteLock and the other is not locked at all.
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class InconsistentPropertyLockingRule extends AbstractAstVisitorRule {
+    String name = 'InconsistentPropertyLocking'
+    int priority = 2
+    Class astVisitorClass = InconsistentPropertyLockingAstVisitor
+}
+
+class InconsistentPropertyLockingAstVisitor extends AbstractAstVisitor {
+
+    private Map<String, MethodNode> guardedMethods = [:]
+    private Map<String, MethodNode> unguardedMethods = [:]
+
+    @Override
+    void visitMethodEx(MethodNode node) {
+        if (node.name.startsWith('get') && node.name.size() > 3 && AstUtil.getParameterNames(node).isEmpty()) {
+            // is a getter
+            def propName = node.name[3..-1]
+            saveMethodInfo(node)
+            addViolationOnMismatch(node.name, "set$propName")
+        }
+
+        if (node.name.startsWith('is') && node.name.size() > 2 && AstUtil.getParameterNames(node).isEmpty()) {
+            // is a getter
+            def propName = node.name[2..-1]
+            saveMethodInfo(node)
+            addViolationOnMismatch(node.name, "set$propName")
+        }
+
+        if (node.name.startsWith('set') && node.name.size() > 3 && AstUtil.getParameterNames(node).size() == 1) {
+            // is a setter
+            def propName = node.name[3..-1]
+            saveMethodInfo(node)
+            addViolationOnMismatch("get$propName", node.name)
+            addViolationOnMismatch("is$propName", node.name)
+        }
+
+        super.visitMethodEx(node)
+    }
+
+    private def saveMethodInfo(MethodNode node) {
+
+        if (isWriteLocked(node) || isReadLocked(node)) {
+            guardedMethods.put(node.name, node)
+        } else {
+            unguardedMethods.put(node.name, node)
+        }
+    }
+
+    void addViolationOnMismatch(String getterName, String setterName) {
+        if (guardedMethods.containsKey(getterName) && unguardedMethods.containsKey(setterName)) {
+            MethodNode unguardedNode = unguardedMethods.get(setterName)
+            MethodNode guardedNode = guardedMethods.get(getterName)
+            def lockType = getGuardName(guardedNode)
+            addViolation(unguardedNode, "The getter method $getterName is marked @$lockType but the setter method $setterName is not locked")
+        }
+        if (unguardedMethods.containsKey(getterName) && guardedMethods.containsKey(setterName)) {
+            MethodNode unguardedNode = unguardedMethods.get(getterName)
+            MethodNode guardedNode = guardedMethods.get(setterName)
+            def lockType = getGuardName(guardedNode)
+            addViolation(unguardedNode, "The setter method $setterName is marked @$lockType but the getter method $getterName is not locked")
+        }
+    }
+
+    private static String getGuardName(MethodNode guardedNode) {
+        if (isWriteLocked(guardedNode)) {
+            return 'WithWriteLock'
+        }
+        if (isReadLocked(guardedNode)) {
+            return 'WithReadLock'
+        }
+        ''
+    }
+
+    private static boolean isWriteLocked(MethodNode node) {
+        AstUtil.getAnnotation(node, 'WithWriteLock') || AstUtil.getAnnotation(node, 'groovy.transform.WithWriteLock')
+    }
+
+    private static boolean isReadLocked(MethodNode node) {
+        AstUtil.getAnnotation(node, 'WithReadLock') || AstUtil.getAnnotation(node, 'groovy.transform.WithReadLock')
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/concurrency/InconsistentPropertyLockingRuleTest.groovy
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for InconsistentPropertyLockingRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class InconsistentPropertyLockingRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "InconsistentPropertyLocking"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            class Person {
+                String name
+                Date birthday
+                boolean deceased
+                boolean parent
+
+                @WithWriteLock setName(String name) {
+                    this.name = name
+                }
+                @groovy.transform.WithReadLock String getName() {
+                    name
+                }
+
+                void setBirthday(Date birthday) {
+                    this.birthday = birthday
+                }
+
+                String getBirthday() {
+                    birthday
+                }
+
+                @groovy.transform.WithWriteLock void setDeceased(boolean deceased) {
+                    this.deceased = deceased
+                }
+
+                @WithReadLock boolean isDeceased() {
+                    deceased
+                }
+
+                @WithWriteLock void setParent(boolean parent) {
+                    this.parent = parent
+                }
+
+                @WithReadLock boolean isParent() {
+                    parent
+                }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testWriteLockStringGetMethod() {
+        final SOURCE = '''
+            class Person {
+                String name
+
+                @WithWriteLock setName(String name) {
+                    this.name = name
+                }
+                // violation, get method should be locked
+                String getName() {
+                    name
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 9, 'String getName()',
+                'The setter method setName is marked @WithWriteLock but the getter method getName is not locked')
+    }
+
+    void testReadLockDateSetMethod() {
+        final SOURCE = '''
+            class Person {
+                Date birthday
+
+                // violation, set method should be locked
+                void setBirthday(Date birthday) {
+                    this.birthday = birthday
+                }
+
+                @groovy.transform.WithReadLock String getBirthday() {
+                    birthday
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 6, 'void setBirthday',
+                'The getter method getBirthday is marked @WithReadLock but the setter method setBirthday is not locked')
+    }
+
+    void testLockedBooleanSetMethod() {
+        final SOURCE = '''
+            class Person {
+                boolean deceased
+
+                // violation, set method should be locked
+                void setDeceased(boolean deceased) {
+                    this.deceased = deceased
+                }
+
+                @WithReadLock boolean isDeceased() {
+                    deceased
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 6, 'void setDeceased(',
+                'The getter method isDeceased is marked @WithReadLock but the setter method setDeceased is not locked')
+    }
+
+    void testWriteLockBooleanGetMethod() {
+        final SOURCE = '''
+            class Person {
+                boolean parent
+
+                @groovy.transform.WithWriteLock void setParent(boolean parent) {
+                    this.parent = parent
+                }
+
+                // violation, get method should be locked
+                boolean isParent() {
+                    parent
+                }
+            }
+        '''
+        assertSingleViolation(SOURCE, 10, 'boolean isParent()',
+                'The setter method setParent is marked @WithWriteLock but the getter method isParent is not locked')
+    }
+
+    protected Rule createRule() {
+        new InconsistentPropertyLockingRule()
+    }
+}
