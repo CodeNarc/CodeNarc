f562477ea676b9721dc66be56f475a6cb37cf40d
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-02 10:03:11 +0000

    added static matcher field rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@587 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/concurrency/StaticMatcherFieldRule.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import java.lang.reflect.Modifier
+import java.util.regex.Matcher
+import org.codehaus.groovy.ast.FieldNode
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * Matcher objects should not be used as static fields. Calendars are inherently unsafe for multithreaded use. Sharing a
+ * single instance across thread boundaries without proper synchronization will result in erratic behavior of the application.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class StaticMatcherFieldRule extends AbstractAstVisitorRule {
+    String name = 'StaticMatcherField'
+    int priority = 2
+    Class astVisitorClass = StaticMatcherFieldAstVisitor
+}
+
+class StaticMatcherFieldAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitFieldEx(FieldNode node) {
+
+        if (Modifier.isStatic(node.modifiers) && AstUtil.classNodeImplementsType(node.type, Matcher)) {
+            addViolation(node, "Matcher instances are not thread safe. Wrap the Matcher field $node.name in a ThreadLocal or make it an instance field")
+        }
+        super.visitFieldEx(node)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/concurrency/StaticMatcherFieldRuleTest.groovy
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for StaticMatcherFieldRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class StaticMatcherFieldRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "StaticMatcherField"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+          // these usages are OK
+          class MyCorrectClass {
+            private Matcher matcher1
+            static ThreadLocal<Matcher> matcher2
+          }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testStaticField() {
+        final SOURCE = '''
+          class MyClass {
+            static Matcher matcher1
+          }
+        '''
+        assertSingleViolation(SOURCE, 3, 'static Matcher matcher1', 'Matcher instances are not thread safe. Wrap the Matcher field matcher1 in a ThreadLocal or make it an instance field')
+    }
+
+    void testStaticFieldFullyQUalifiedName() {
+        final SOURCE = '''
+          class MyClass {
+            static java.util.regex.Matcher matcher2
+          }
+        '''
+        assertSingleViolation(SOURCE, 3, 'static java.util.regex.Matcher matcher2', 'Matcher instances are not thread safe. Wrap the Matcher field matcher2 in a ThreadLocal or make it an instance field')
+    }
+
+    protected Rule createRule() {
+        new StaticMatcherFieldRule()
+    }
+}
