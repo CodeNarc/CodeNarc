0b253ee7c736c02062954e28111783249a6873b5
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-02-03 13:01:44 +0000

    avoid busy wait rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@591 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/concurrency/AvoidBusyWaitRule.groovy
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.stmt.BlockStatement
+import org.codehaus.groovy.ast.stmt.ExpressionStatement
+import org.codehaus.groovy.ast.stmt.WhileStatement
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.stmt.ForStatement
+
+/**
+ * Busy waiting (forcing a Thread.sleep() while waiting on a condition) should be avoided. Prefer using the gate and barrier objects in the java.util.concurrent package. 
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class AvoidBusyWaitRule extends AbstractAstVisitorRule {
+    String name = 'AvoidBusyWait'
+    int priority = 2
+    Class astVisitorClass = AvoidBusyWaitAstVisitor
+}
+
+class AvoidBusyWaitAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitWhileLoop(WhileStatement node) {
+        addViolationIfBusyWait(node)
+        super.visitWhileLoop(node)
+    }
+
+    @Override
+    void visitForLoop(ForStatement node) {
+        // -1 is line number for a for-each loop
+        if (node.variable.lineNumber == -1) {
+            addViolationIfBusyWait(node)
+        }
+        super.visitForLoop(node)
+    }
+
+    private def addViolationIfBusyWait(node) {
+        if (node.loopBlock instanceof BlockStatement
+                && node.loopBlock.statements?.size() == 1
+                && node.loopBlock.statements[0] instanceof ExpressionStatement) {
+
+            Expression expression = node.loopBlock.statements[0].expression
+            if (AstUtil.isMethodCall(expression, 'sleep', 1) || AstUtil.isMethodCall(expression, 'sleep', 2)) {
+                addViolation(expression, 'Busy wait detected. Switch the usage of Thread.sleep() to a lock or gate from java.util.concurrent')
+            }
+        }
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/concurrency/AvoidBusyWaitRuleTest.groovy
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.concurrency
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for AvoidBusyWaitRule
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class AvoidBusyWaitRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == "AvoidBusyWait"
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            // here is the proper way to wait:
+            countDownLatch.await()
+
+            // this is weird code to write, but does not cause a violation
+            // sleep inside a for-each loop is OK for some reason
+            for (def x : collections) {
+                sleep(1000)
+            }
+
+            for (def x = 0; x < 10; x++) {
+                sleep(1000, 1, 3)   // sleep does not take 3 parameters
+            }
+
+            while (x) {
+                // you should use a lock here, but technically you are
+                // not just busy waiting because you are doing other work
+                doSomething()
+                sleep(1000) 
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testWhileLoop() {
+        final SOURCE = '''
+            while (x) {
+                Thread.sleep(1000)
+            }
+        '''
+        assertSingleViolation(SOURCE, 3, 'Thread.sleep(1000)', 'Busy wait detected. Switch the usage of Thread.sleep() to a lock or gate from java.util.concurrent')
+    }
+
+    void testWhileLoopWithClosure() {
+        final SOURCE = '''
+            while (x) {
+                sleep(1000) { /* interruption handler */}
+            }
+        '''
+        assertSingleViolation(SOURCE, 3, 'sleep(1000)', 'Busy wait detected. Switch the usage of Thread.sleep() to a lock or gate from java.util.concurrent')
+    }
+
+    void testForLoop() {
+        final SOURCE = '''
+            for (int x = 10; x; x--) {
+                sleep(1000)     // sleep is added to Object in Groovy
+            }
+        '''
+        assertSingleViolation(SOURCE, 3, 'sleep(1000)', 'Busy wait detected. Switch the usage of Thread.sleep() to a lock or gate from java.util.concurrent')
+    }
+
+    protected Rule createRule() {
+        new AvoidBusyWaitRule()
+    }
+}
