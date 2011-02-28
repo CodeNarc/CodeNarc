73b4e54c5117db0487b4a22cf766526f28b778e1
Chris Mair <chrismair@earthlink.net>
2011-02-28 02:59:03 +0000

    Enable configuring rules in a ruleset using rule name only; generation scripts
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@621 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/site/resources/SampleRuleSet-AllRulesByRuleSetName.groovy
@@ -0,0 +1,246 @@
+ruleset {
+
+    description '''
+        A Sample Groovy RuleSet containing all CodeNarc Rules, grouped by RuleSet name.
+        You can use this as a template for your own custom RuleSet.
+        Just delete the rules that you don't want to include.
+        '''
+
+    // rulesets/basic.xml
+    AddEmptyString
+    AssignmentInConditional
+    BigDecimalInstantiation
+    BooleanGetBoolean
+    BooleanMethodReturnsNull
+    BrokenOddnessCheck
+    CloneableWithoutClone
+    CompareToWithoutComparable
+    ConfusingTernary
+    ConsecutiveLiteralAppends
+    ConsecutiveStringConcatenation
+    ConstantIfExpression
+    ConstantTernaryExpression
+    DeadCode
+    DoubleNegative
+    DuplicateCaseStatement
+    EmptyCatchBlock
+    EmptyElseBlock
+    EmptyFinallyBlock
+    EmptyForStatement
+    EmptyIfStatement
+    EmptyInstanceInitializer
+    EmptyMethod
+    EmptyStaticInitializer
+    EmptySwitchStatement
+    EmptySynchronizedStatement
+    EmptyTryBlock
+    EmptyWhileStatement
+    EqualsAndHashCode
+    ExplicitArrayListInstantiation
+    ExplicitCallToAndMethod
+    ExplicitCallToCompareToMethod
+    ExplicitCallToDivMethod
+    ExplicitCallToEqualsMethod
+    ExplicitCallToGetAtMethod
+    ExplicitCallToLeftShiftMethod
+    ExplicitCallToMinusMethod
+    ExplicitCallToModMethod
+    ExplicitCallToMultiplyMethod
+    ExplicitCallToOrMethod
+    ExplicitCallToPlusMethod
+    ExplicitCallToPowerMethod
+    ExplicitCallToRightShiftMethod
+    ExplicitCallToXorMethod
+    ExplicitGarbageCollection
+    ExplicitHashMapInstantiation
+    ExplicitHashSetInstantiation
+    ExplicitLinkedListInstantiation
+    ExplicitStackInstantiation
+    ExplicitTreeSetInstantiation
+    GStringAsMapKey
+    GroovyLangImmutable
+    IntegerGetInteger
+    InvertedIfElse
+    RemoveAllOnSelf
+    ReturnFromFinallyBlock
+    ReturnsNullInsteadOfEmptyArray
+    ReturnsNullInsteadOfEmptyCollection
+    SerialVersionUID
+    SerializableClassMustDefineSerialVersionUID
+    SimpleDateFormatMissingLocale
+    ThrowExceptionFromFinallyBlock
+    
+    // rulesets/braces.xml
+    ElseBlockBraces
+    ForStatementBraces
+    IfStatementBraces
+    WhileStatementBraces
+    
+    // rulesets/concurrency.xml
+    BusyWait
+    DoubleCheckedLocking
+    InconsistentPropertyLocking
+    InconsistentPropertySynchronization
+    NestedSynchronization
+    StaticCalendarField
+    StaticDateFormatField
+    StaticMatcherField
+    SynchronizedMethod
+    SynchronizedOnBoxedPrimitive
+    SynchronizedOnGetClass
+    SynchronizedOnReentrantLock
+    SynchronizedOnString
+    SynchronizedOnThis
+    SynchronizedReadObjectMethod
+    SystemRunFinalizersOnExit
+    ThreadGroup
+    ThreadLocalNotStaticFinal
+    ThreadYield
+    UseOfNotifyMethod
+    VolatileArrayField
+    VolatileLongOrDoubleField
+    WaitOutsideOfWhileLoop
+    
+    // rulesets/design.xml
+    AbstractClassWithoutAbstractMethod
+    CloseWithoutCloseable
+    ConstantsOnlyInterface
+    EmptyMethodInAbstractClass
+    FinalClassWithProtectedMember
+    ImplementationAsType
+    
+    // rulesets/dry.xml
+    DuplicateNumberLiteral
+    DuplicateStringLiteral
+    
+    // rulesets/exceptions.xml
+    CatchArrayIndexOutOfBoundsException
+    CatchError
+    CatchException
+    CatchIllegalMonitorStateException
+    CatchIndexOutOfBoundsException
+    CatchNullPointerException
+    CatchRuntimeException
+    CatchThrowable
+    ConfusingClassNamedException
+    ExceptionExtendsError
+    MissingNewInThrowStatement
+    ReturnNullFromCatchBlock
+    ThrowError
+    ThrowException
+    ThrowNullPointerException
+    ThrowRuntimeException
+    ThrowThrowable
+    
+    // rulesets/generic.xml
+    IllegalRegex
+    RequiredRegex
+    StatelessClass
+    
+    // rulesets/grails.xml
+    GrailsPublicControllerMethod
+    GrailsServletContextReference
+    GrailsSessionReference
+    GrailsStatelessService
+    
+    // rulesets/imports.xml
+    DuplicateImport
+    ImportFromSamePackage
+    UnnecessaryGroovyImport
+    UnusedImport
+    
+    // rulesets/logging.xml
+    LoggerForDifferentClass
+    LoggerWithWrongModifiers
+    LoggingSwallowsStacktrace
+    MultipleLoggers
+    PrintStackTrace
+    Println
+    SystemErrPrint
+    SystemOutPrint
+    
+    // rulesets/junit.xml
+    ChainedTest
+    CoupledTestCase
+    JUnitAssertAlwaysFails
+    JUnitAssertAlwaysSucceeds
+    JUnitFailWithoutMessage
+    JUnitPublicNonTestMethod
+    JUnitSetUpCallsSuper
+    JUnitStyleAssertions
+    JUnitTearDownCallsSuper
+    JUnitTestMethodWithoutAssert
+    JUnitUnnecessarySetUp
+    JUnitUnnecessaryTearDown
+    UnnecessaryFail
+    UseAssertEqualsInsteadOfAssertTrue
+    UseAssertFalseInsteadOfNegation
+    UseAssertNullInsteadOfAssertEquals
+    UseAssertSameInsteadOfAssertTrue
+    UseAssertTrueInsteadOfAssertEquals
+    UseAssertTrueInsteadOfNegation
+    
+    // rulesets/naming.xml
+    AbstractClassName
+    ClassName
+    ConfusingMethodName
+    FieldName
+    InterfaceName
+    MethodName
+    ObjectOverrideMisspelledMethodName
+    PackageName
+    ParameterName
+    PropertyName
+    VariableName
+    
+    // rulesets/size.xml
+    AbcComplexity
+    ClassSize
+    CyclomaticComplexity
+    MethodCount
+    MethodSize
+    NestedBlockDepth
+    
+    // rulesets/unnecessary.xml
+    UnnecessaryBigDecimalInstantiation
+    UnnecessaryBigIntegerInstantiation
+    UnnecessaryBooleanExpression
+    UnnecessaryBooleanInstantiation
+    UnnecessaryCallForLastElement
+    UnnecessaryCallToSubstring
+    UnnecessaryCatchBlock
+    UnnecessaryCollectCall
+    UnnecessaryCollectionCall
+    UnnecessaryConstructor
+    UnnecessaryDefInMethodDeclaration
+    UnnecessaryDoubleInstantiation
+    UnnecessaryFloatInstantiation
+    UnnecessaryGString
+    UnnecessaryGetter
+    UnnecessaryIfStatement
+    UnnecessaryInstantiationToGetClass
+    UnnecessaryIntegerInstantiation
+    UnnecessaryLongInstantiation
+    UnnecessaryModOne
+    UnnecessaryNullCheck
+    UnnecessaryNullCheckBeforeInstanceOf
+    UnnecessaryObjectReferences
+    UnnecessaryOverridingMethod
+    UnnecessaryPublicModifier
+    UnnecessaryReturnKeyword
+    UnnecessarySelfAssignment
+    UnnecessarySemicolon
+    UnnecessaryStringInstantiation
+    UnnecessaryTernaryExpression
+    UnnecessaryTransientModifier
+    
+    // rulesets/unused.xml
+    UnusedArray
+    UnusedObject
+    UnusedPrivateField
+    UnusedPrivateMethod
+    UnusedPrivateMethodParameter
+    UnusedVariable
+    
+    
+}
