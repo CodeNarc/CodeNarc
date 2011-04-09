51d4d5f2eb6ae415fa5447d8adb61b2c3ecd9e5a
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-04-09 15:55:55 +0000

    new rule: duplicate constant value in set literal
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@669 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/DuplicateSetValueRule.groovy
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codehaus.groovy.ast.expr.CastExpression
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.ListExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.ASTNode
+
+/**
+ * A Set literal is created with duplicate constant value. A set cannot contain two elements with the same value.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class DuplicateSetValueRule extends AbstractAstVisitorRule {
+    String name = 'DuplicateSetValue'
+    int priority = 2
+    Class astVisitorClass = DuplicateSetValueAstVisitor
+}
+
+class DuplicateSetValueAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitCastExpression(CastExpression expression) {
+
+        if (isSetLiteral(expression)) {
+            expression.expression.expressions?.inject([]) { acc, value ->
+                if (isDuplicate(value, acc)) {
+                    addViolationForDuplicate(value)
+                }
+                acc
+            }
+        }
+
+        super.visitCastExpression(expression)
+    }
+
+    private static boolean isDuplicate(ASTNode expression, previousValues) {
+        if ((expression instanceof ConstantExpression)) {
+            if (previousValues.contains(expression.value)) {
+                return true
+            } else {
+                previousValues.add(expression.value)
+            }
+        }
+        false
+    }
+
+    private addViolationForDuplicate(ConstantExpression constant) {
+        if (constant.value == null) {
+            addViolation(constant, 'The constant value null is duplicated in the Set literal')
+        } else if (constant.value instanceof String) {
+            addViolation(constant, "The constant value '$constant.value' is duplicated in the Set literal")
+        } else {
+            addViolation(constant, "The constant value $constant.value is duplicated in the Set literal")
+        }
+    }
+
+    private static boolean isSetLiteral(CastExpression expression) {
+        return expression.type?.name?.endsWith('Set') && expression.expression instanceof ListExpression
+    }
+
+
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/DuplicateSetValueRuleTest.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for DuplicateSetValueRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class DuplicateSetValueRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'DuplicateSetValue'
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            def a = [1, 2, 3, 4, null] as Set
+            def b = ['1', '2', '3', null, '4'] as Set
+            def c = [1, '1', null] as Set
+            def d = [1, 1, 1] as ArrayList
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testDuplicateIntegers() {
+        final SOURCE = '''
+            def a = [1, 2, null, 2, 4] as Set
+            def b = [1, 2, 2, 4] as HashSet
+            def c = [1, 3, null, 3, 4] as SortedSet
+            def d = [1, null, null, 3, 4] as SortedSet
+        '''
+        assertViolations(SOURCE,
+                [lineNumber: 2, sourceLineText: 'def a = [1, 2, null, 2, 4] as Set', messageText: 'The constant value 2 is duplicated in the Set literal'],
+                [lineNumber: 3, sourceLineText: 'def b = [1, 2, 2, 4] as HashSet', messageText: 'The constant value 2 is duplicated in the Set literal'],
+                [lineNumber: 4, sourceLineText: 'def c = [1, 3, null, 3, 4] as SortedSet', messageText: 'The constant value 3 is duplicated in the Set literal'],
+                [lineNumber: 5, sourceLineText: 'def d = [1, null, null, 3, 4] as SortedSet', messageText: 'The constant value null is duplicated in the Set literal'])
+    }
+
+    void testDuplicateStrings() {
+        final SOURCE = '''
+            def e = ['1', '2', '2', null, '4'] as Set
+            def f = ['1', '2', '2', null, '4'] as java.util.HashSet
+            def g = ['1', '3', '3', null, '4'] as SortedSet
+        '''
+        assertViolations(SOURCE,
+                [lineNumber: 2, sourceLineText: "def e = ['1', '2', '2', null, '4'] as Set", messageText: "The constant value '2' is duplicated in the Set literal"],
+                [lineNumber: 3, sourceLineText: "def f = ['1', '2', '2', null, '4'] as java.util.HashSet", messageText: "The constant value '2' is duplicated in the Set literal"],
+                [lineNumber: 4, sourceLineText: "def g = ['1', '3', '3', null, '4'] as SortedSet", messageText: "The constant value '3' is duplicated in the Set literal"])
+    }
+
+    void testDuplicateIntsInCustomType() {
+        final SOURCE = '''
+            def d = [1, 2, 2, 4] as FooSet
+            def h = ['1', '2', '2', '4'] as FooSet
+        '''
+        assertViolations(SOURCE,
+                [lineNumber: 2, sourceLineText: 'def d = [1, 2, 2, 4] as FooSet', messageText: 'The constant value 2 is duplicated in the Set literal'],
+                [lineNumber: 3, sourceLineText: "def h = ['1', '2', '2', '4'] as FooSet", messageText: "The constant value '2' is duplicated in the Set literal"])
+    }
+
+    protected Rule createRule() {
+        new DuplicateSetValueRule()
+    }
+}
