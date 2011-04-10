47c1f6a3dafb6e51bc4cac3a85401fcf073b8f18
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-04-10 14:37:19 +0000

    new rule: direct connection management
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@674 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/security/DirectConnectionManagementRule.groovy
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.security
+
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * The J2EE standard requires that applications use the container's resource management facilities to obtain connections to resources. Every major web application container provides pooled database connection management as part of its resource management framework. Duplicating this functionality in an application is difficult and error prone, which is part of the reason it is forbidden under the J2EE standard.
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class DirectConnectionManagementRule extends AbstractAstVisitorRule {
+    String name = 'DirectConnectionManagement'
+    int priority = 2
+    Class astVisitorClass = DirectConnectionManagementAstVisitor
+}
+
+class DirectConnectionManagementAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+
+        if (AstUtil.isMethodCall(call, 'DriverManager', 'getConnection')) {
+            addViolation(call, 'Using DriverManager.getConnection() violates the J2EE standards. Use the connection from the context instead')
+        } else if (AstUtil.isMethodNamed(call, 'getConnection') && isJavaSQLDriverManagerCall(call)) {
+            addViolation(call, 'Using DriverManager.getConnection() violates the J2EE standards. Use the connection from the context instead')
+        }
+
+        super.visitMethodCallExpression(call)
+    }
+
+    private static boolean isJavaSQLDriverManagerCall(MethodCallExpression call) {
+        if (AstUtil.isPropertyNamed(call.objectExpression, 'DriverManager')) {
+            if (AstUtil.isPropertyNamed(call.objectExpression.objectExpression, 'sql')) {
+                if (AstUtil.isVariable(call.objectExpression.objectExpression.objectExpression, 'java')) {
+                    return true
+                }
+            }
+        }
+        false
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/security/DirectConnectionManagementRuleTest.groovy
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.security
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for DirectConnectionManagementRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class DirectConnectionManagementRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'DirectConnectionManagement'
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            OtherManager.getConnection()
+            java.something.DriverManager.getConnection()
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testViolations() {
+        final SOURCE = '''
+            DriverManager.getConnection()
+            java.sql.DriverManager.getConnection()
+        '''
+        assertTwoViolations(SOURCE,
+                2, 'DriverManager.getConnection()', 'Using DriverManager.getConnection() violates the J2EE standards. Use the connection from the context instead',
+                3, 'java.sql.DriverManager.getConnection()', 'Using DriverManager.getConnection() violates the J2EE standards. Use the connection from the context instead')
+    }
+
+    protected Rule createRule() {
+        new DirectConnectionManagementRule()
+    }
+}
