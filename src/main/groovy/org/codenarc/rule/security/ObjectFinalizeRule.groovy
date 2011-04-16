3e1a3a378ea35f1e63782576828613cfda81e437
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-04-16 19:31:23 +0000

    new rule: explicit call to finalize()
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@684 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/security/ObjectFinalizeRule.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.security
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+
+/**
+ * The finalize() method should only be called by the JVM after the object has been garbage collected.
+ *
+ * @author 'Hamlet D'Arcy'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class ObjectFinalizeRule extends AbstractAstVisitorRule {
+    String name = 'ObjectFinalize'
+    int priority = 2
+    Class astVisitorClass = ObjectFinalizeAstVisitor
+}
+
+class ObjectFinalizeAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+
+        if (AstUtil.isMethodNamed(call, 'finalize', 0)) {
+            addViolation(call, 'The finalize() method should only be called by the JVM after the object has been garbage collected')
+        }
+        super.visitMethodCallExpression(call)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/security/ObjectFinalizeRuleTest.groovy
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.security
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for ObjectFinalizeRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class ObjectFinalizeRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'ObjectFinalize'
+    }
+
+    void testApplyTo_Violation_Initializers() {
+        final SOURCE = '''
+            class MyClass {
+                static {
+                    this.finalize()
+                }
+                {
+                    widget.finalize()
+                }
+            }
+        '''
+        assertTwoViolations(SOURCE,
+                4, 'this.finalize()', 'The finalize() method should only be called by the JVM after the object has been garbage collected',
+                7, 'widget.finalize()', 'The finalize() method should only be called by the JVM after the object has been garbage collected')
+    }
+
+    void testApplyTo_Violation_Methods() {
+        final SOURCE = '''
+            class MyClass {
+                static def method1() {
+                    foo.finalize()
+                }
+                def method2() {
+                    property.finalize()
+                }
+            }
+        '''
+        assertTwoViolations(SOURCE,
+                4, 'foo.finalize()', 'The finalize() method should only be called by the JVM after the object has been garbage collected',
+                7, 'property.finalize()', 'The finalize() method should only be called by the JVM after the object has been garbage collected')
+    }
+
+    void testApplyTo_Violation_Closures() {
+        final SOURCE = '''
+            File.finalize()
+            def method = {
+                file.finalize()
+            }
+        '''
+        assertTwoViolations(SOURCE,
+                2, 'File.finalize()',
+                4, 'file.finalize()')
+    }
+
+    void testApplyTo_NoViolations() {
+        final SOURCE = '''class MyClass {
+                def myMethod() {
+                    widget.Finalize()
+                    widget.finalize('')
+                }
+            }'''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        new ObjectFinalizeRule()
+    }
+}
