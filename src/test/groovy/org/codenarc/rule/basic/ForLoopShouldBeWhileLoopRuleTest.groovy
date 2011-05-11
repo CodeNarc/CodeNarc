e70d8459e16ed25196288068dce345432425bcdf
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-05-11 06:40:41 +0000

    ForLoopCanBeWhileLoop Rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@704 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/test/groovy/org/codenarc/rule/basic/ForLoopShouldBeWhileLoopRuleTest.groovy
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for ForLoopShouldBeWhileLoopRule
+ *
+ * @author 'Victor Savkin'
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class ForLoopShouldBeWhileLoopRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 5
+        assert rule.name == 'ForLoopShouldBeWhileLoop'
+    }
+
+    void testShouldNotAddViolationsForWhileLoops() {
+        final SOURCE = '''
+        	while(1 > 2)
+               println "never happen"
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldNotAddViolationsForNewLoops() {
+        final SOURCE = '''
+        	for(i in [1,2])
+               println i
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldNotAddViolationsIfForLoopHasInitExpr() {
+        final SOURCE = '''
+        	for(int i = 0; i<5;)
+                println i++
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldNotAddViolationsIfForLoopHasUpdateExpr() {
+        final SOURCE = '''
+            int i = 0
+        	for(; i < 5; i++)
+                println i
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddViolationIfForLoopHasOnlyConditionExpr() {
+        final SOURCE = '''
+            int i = 0
+        	for(; i < 5;)
+                println i++
+        '''
+        assertSingleViolation SOURCE, 3, 'for(; i < 5;)', 'The for loop can be simplified to a while loop'
+    }
+    
+    protected Rule createRule() {
+        new ForLoopShouldBeWhileLoopRule()
+    }
+}
