d8b0f41f168a7398853f2c24a3953cfd1b735c44
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-05-18 10:42:01 +0000

    swallow thread death rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@710 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/exceptions/SwallowThreadDeathRule.groovy
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.exceptions
+
+import org.codehaus.groovy.ast.expr.ConstructorCallExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.ast.stmt.CatchStatement
+import org.codehaus.groovy.ast.stmt.ThrowStatement
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * Detects code that catches java.lang.ThreadDeath without re-throwing it. If ThreadDeath is caught by a method, it is important that it be rethrown so that the thread actually dies.
+ *
+ * @author Rob Fletcher
+ * @author Klaus Baumecker
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class SwallowThreadDeathRule extends AbstractAstVisitorRule {
+    String name = 'SwallowThreadDeath'
+    int priority = 2
+    Class astVisitorClass = SwallowThreadDeathAstVisitor
+}
+
+class SwallowThreadDeathAstVisitor extends AbstractAstVisitor {
+
+    private boolean withinCatchThreadDeath = false
+    private String caughtVariableName = null
+    private boolean foundThrow = false
+
+    @Override
+    void visitCatchStatement(CatchStatement statement) {
+        if (statement.exceptionType.name == ThreadDeath.simpleName) {
+            withinCatchThreadDeath = true
+            foundThrow = false
+            caughtVariableName = statement.variable.name
+            super.visitCatchStatement(statement)
+            withinCatchThreadDeath = false
+            if (!foundThrow) {
+                addViolation statement, 'ThreadDeath caught without being re-thrown. If ThreadDeath is caught by a method, it is important that it be rethrown so that the thread actually dies.'
+            }
+        }
+    }
+
+    @Override
+    void visitThrowStatement(ThrowStatement statement) {
+        if (withinCatchThreadDeath) {
+            def expression = statement.expression
+            if (expression instanceof VariableExpression && expression.name == caughtVariableName) {
+                foundThrow = true
+            } else if (expression instanceof ConstructorCallExpression && expression.type.name == ThreadDeath.simpleName) {
+                foundThrow = true
+            }
+        }
+    }
+
+
+}
---
+++ src/test/groovy/org/codenarc/rule/exceptions/SwallowThreadDeathRuleTest.groovy
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.exceptions
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for SwallowThreadDeathRule
+ *
+ * @author Rob Fletcher
+ * @author Klaus Baumecker
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class SwallowThreadDeathRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'SwallowThreadDeath'
+    }
+
+    void testRethrowingCaughtErrorIsLegal() {
+        final SOURCE = '''
+        	try {
+                def a = 0
+            } catch (ThreadDeath td) {
+                throw td
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testReThrowingAnotherInstanceOfThreadDeathIsLegal() {
+        final SOURCE = '''
+        	try {
+                def a = 0
+            } catch (ThreadDeath td) {
+                throw new ThreadDeath()
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testNesting() {
+        final SOURCE = '''
+        	try {
+                def a = 0
+            } catch (ThreadDeath td) {
+                try { println 4 } catch (ThreadDeath ttdd) { throw ttdd }
+                throw td
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testCatchingWithoutRethrowingAnythingIsAViolation() {
+        final SOURCE = '''
+        	try {
+                def a = 0
+            } catch (ThreadDeath td) {
+                td.printStackTrace()
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, '} catch (ThreadDeath td) {')
+    }
+
+    void testCatchingAndThrowingAnotherVariableIsAViolation() {
+        final SOURCE = '''
+            def other = new ThreadDeath()
+        	try {
+                def a = 0
+            } catch (ThreadDeath td) {
+                throw other
+            }
+        '''
+        assertSingleViolation(SOURCE, 5, '} catch (ThreadDeath td) {')
+    }
+
+    void testCatchingAndThrowingSomethingElseIsAViolation() {
+        final SOURCE = '''
+        	try {
+                def a = 0
+            } catch (ThreadDeath td) {
+                throw new RuntimeException("o noes")
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, '} catch (ThreadDeath td) {')
+    }
+
+    protected Rule createRule() {
+        new SwallowThreadDeathRule()
+    }
+}
