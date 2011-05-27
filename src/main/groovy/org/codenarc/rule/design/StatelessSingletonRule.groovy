52e2e34afc33da8aa2196aeac88a492627607c8c
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-05-27 07:12:31 +0000

    Stateless Singleton Rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@723 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/design/StatelessSingletonRule.groovy
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codenarc.rule.design
+
+import org.codehaus.groovy.ast.ClassNode
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * There is no benefit in creating a stateless Singleton. Make a new instance with the new keyword instead.
+ *
+ * @author 'Victor Savkin'
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class StatelessSingletonRule extends AbstractAstVisitorRule {
+    String name = 'StatelessSingleton'
+    int priority = 5
+    Class astVisitorClass = StatelessSingletonAstVisitor
+    String instanceRegex = 'instance|_instance'
+}
+
+class StatelessSingletonAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    protected void visitClassComplete(ClassNode node) {
+        if (isSingleton(node) && !doesExtendClass(node) && !hasState(node)) {
+            addViolation node, 'There is no point in creating a stateless Singleton. ' +
+                "Make a new instance of '${node.nameWithoutPackage}' with the new keyword instead."
+        }
+        super.visitClassComplete node
+    }
+
+    private isSingleton(classNode) {
+        hasSingletonAnnotation(classNode) ||
+            hasOneStaticFieldOfItself(classNode) ||
+            hasOneStaticFieldNamedInstance(classNode)
+    }
+
+    private static doesExtendClass(classNode) {
+        classNode.superClass.name != Object.name
+    }
+
+    private hasState(classNode) {
+        classNode.fields.any {
+            it.type.name != classNode.name && !(it.name ==~ rule.instanceRegex)
+        }
+    }
+
+    private static hasOneStaticFieldOfItself(classNode) {
+        classNode.fields.findAll {
+            it.static && it.type.name == classNode.name
+        }.size() == 1
+    }
+
+    private hasOneStaticFieldNamedInstance(classNode) {
+        classNode.fields.findAll {
+            it.static && it.type.name == Object.name && it.name ==~ rule.instanceRegex
+        }.size() == 1
+    }
+
+    private static hasSingletonAnnotation(classNode) {
+        classNode?.annotations?.any {it.classNode.name == Singleton.name}
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/design/StatelessSingletonRuleTest.groovy
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for StatelessSingletonRule
+ *
+ * @author Victor Savkin
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class StatelessSingletonRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 5
+        assert rule.name == 'StatelessSingleton'
+    }
+
+    void testShouldAddNoViolationsForStatelessClassThatIsNotSingleton() {
+        final SOURCE = '''
+            class Service {
+                void processItem(item){
+                }
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddNoViolationsForStatefulClassThatIsNotSingleton() {
+        final SOURCE = '''
+            class Service {
+                def state
+
+                void processItem(item){
+                }
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddNoViolationsForStatefulClassAnnotatedAsSingleton() {
+        final SOURCE = '''
+            @groovy.lang.Singleton
+            class Service {
+                def state
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddNoViolationsForStatefulClassImplementingSingletonPattern() {
+        final SOURCE = '''
+            class Service {
+                static Service instance = new Service()
+                def state
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddViolationForClassAnnotatedAsSingletonThatDoesNotHaveState() {
+        final SOURCE = '''
+            @groovy.lang.Singleton
+            class Service {
+                void processItem(item){
+                }
+            }
+        '''
+        assertSingleViolation SOURCE, 3, 'class Service', createErrorMsgForClass('Service')
+    }
+
+    void testShouldAddNoViolationsForClassAnnotatedAsSingletonExtendingAnotherClass() {
+        final SOURCE = '''
+            class Parent {}
+
+            @groovy.lang.Singleton
+            class Service extends Parent {
+                void processItem(item){
+                }
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddViolationForClassHavingOneStaticFieldOfItsOwnType() {
+        final SOURCE = '''
+            class Service {
+                static Service srv
+                void processItem(item){
+                }
+            }
+        '''
+        assertSingleViolation SOURCE, 2, 'class Service', createErrorMsgForClass('Service')
+    }
+
+    void testShouldAddViolationForClassHavingOneStaticFieldNamedInstance() {
+        final SOURCE = '''
+            class Service {
+                static instance
+                void processItem(item){
+                }
+            }
+        '''
+        assertSingleViolation SOURCE, 2, 'class Service', createErrorMsgForClass('Service')
+    }
+
+    void testShouldAddViolationForClassHavingOneStaticFieldNamed_Instance() {
+        final SOURCE = '''
+            class Service {
+                static _instance
+                void processItem(item){
+                }
+            }
+        '''
+        assertSingleViolation SOURCE, 2, 'class Service', createErrorMsgForClass('Service')
+    }
+
+    void testShouldAddNoViolationsForClassHavingStaticFieldNamedInstanceOfDifferentType() {
+        final SOURCE = '''
+            class Service {
+                static String instance
+                void processItem(item){
+                }
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddNoViolationsForClassImplementingSingletonPatternAndExtendingAnotherClass() {
+        final SOURCE = '''
+            class Parent {}
+
+            class Service extends Parent {
+                static Service srv
+                void processItem(item){
+                }
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    void testShouldAddNoViolationsForClassHaving2StaticFieldsOfItsOwnType() {
+        final SOURCE = '''
+            class Service {
+                static Service srv1, srv1
+                void processItem(item){
+                }
+            }
+        '''
+        assertNoViolations SOURCE
+    }
+
+    protected Rule createRule() {
+        new StatelessSingletonRule()
+    }
+
+    private createErrorMsgForClass(name){
+        "There is no point in creating a stateless Singleton. Make a new instance of '$name' with the new keyword instead."
+    }
+}
