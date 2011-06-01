525846b028456050bca2095d067accd87a6a0b8d
Chris Mair <chrismair@earthlink.net>
2011-06-01 02:18:44 +0000

    Move StaticConnectionRule into concurrency ruleset
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@737 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

--- src/test/groovy/org/codenarc/rule/concurrency/StaticConnectionRuleTest.groovy
+++ src/test/groovy/org/codenarc/rule/concurrency/StaticConnectionRuleTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.codenarc.rule.security
+package org.codenarc.rule.concurrency
 
 import org.codenarc.rule.AbstractRuleTestCase
 import org.codenarc.rule.Rule
