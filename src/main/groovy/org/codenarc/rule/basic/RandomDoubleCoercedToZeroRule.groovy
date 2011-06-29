401bba41e1185be02a4e2158cb0750e1086ce058
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-06-29 14:14:52 +0000

    Math.random() coerced to non floating point primitive rule.
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@787 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/RandomDoubleCoercedToZeroRule.groovy
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codehaus.groovy.ast.FieldNode
+import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.expr.CastExpression
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.expr.TernaryExpression
+import org.codehaus.groovy.ast.stmt.ReturnStatement
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * The Math.random() method returns a double result greater than or equal to 0.0 and less than 1.0. If you coerce this result into an Integer or int, then it is coerced to zero. Casting the result to int, or assigning it to an int field is probably a bug. 
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class RandomDoubleCoercedToZeroRule extends AbstractAstVisitorRule {
+    String name = 'RandomDoubleCoercedToZero'
+    int priority = 2
+    Class astVisitorClass = RandomDoubleCoercedToZeroAstVisitor
+}
+
+class RandomDoubleCoercedToZeroAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitCastExpression(CastExpression expression) {
+        if (isFirstVisit(expression)) {
+            if (AstUtil.isMethodCall(expression.expression, 'Math', 'random', 0)) {
+                if (expression.type?.name == 'long' || expression.type?.name == 'Long') {
+                    addViolation(expression, "Casting the result of Math.random() to a ${expression.type.name} always results in 0")
+                } else if (expression.type?.name == 'int' || expression.type?.name == 'Integer') {
+                    addViolation(expression, "Casting the result of Math.random() to an ${expression.type.name} always results in 0")
+                }
+            }
+        }
+        super.visitCastExpression(expression)
+    }
+
+    @Override
+    void visitFieldEx(FieldNode node) {
+
+        if (isFirstVisit(node)) {
+            if (node.initialExpression && AstUtil.isMethodCall(node.initialExpression, 'Math', 'random', 0)) {
+                if (node.type?.name == 'long' || node.type?.name == 'Long') {
+                    addViolation(node, "Assigning the result of Math.random() to a ${node.type.name} always results in 0")
+                } else if (node.type?.name == 'int' || node.type?.name == 'Integer') {
+                    addViolation(node, "Assigning the result of Math.random() to an ${node.type.name} always results in 0")
+                }
+            }
+        }
+    }
+
+    @Override
+    void visitMethodEx(MethodNode node) {
+        if (isFirstVisit(node)) {
+            if (node.returnType.name in ['int', 'Integer']) {
+                node.code.visit(new MathRandomTracker(callbackFunction: {
+                    addViolation(it, "Returning the result of Math.random() from an ${node.returnType.name}-returning method always returns 0")
+                }))
+            }
+            if (node.returnType.name in ['long', 'Long']) {
+                node.code.visit(new MathRandomTracker(callbackFunction: {
+                    addViolation(it, "Returning the result of Math.random() from a ${node.returnType.name}-returning method always returns 0")
+                }))
+            }
+        }
+    }
+}
+
+class MathRandomTracker extends AbstractAstVisitor {
+    def callbackFunction
+
+    def void visitReturnStatement(ReturnStatement statement) {
+        callBackForMathRandomReturns(statement.expression)
+        super.visitReturnStatement(statement)
+    }
+
+    private callBackForMathRandomReturns(Expression exp) {
+        def stack = [exp] as Stack
+        while (stack) {
+            exp = stack.pop()
+            if (AstUtil.isMethodCall(exp, 'Math', 'random', 0)) {
+                callbackFunction(exp)
+            } else if (exp instanceof TernaryExpression) {
+                stack.push(exp.trueExpression)
+                stack.push(exp.falseExpression)
+            }
+        }
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/RandomDoubleCoercedToZeroRuleTest.groovy
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for RandomDoubleCoercedToZeroRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class RandomDoubleCoercedToZeroRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'RandomDoubleCoercedToZero'
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+            class MyClass {
+                def a = (double) Math.random()
+                def b = (Double) Math.random()
+                double c = Math.random()
+                Double d = Math.random()
+                def e = (Math.random()) as double
+                def f = (Math.random()) as Double
+                def g = Math.random()
+            }
+
+            Double intMethod() {
+                if (foo) return Math.random()
+            }
+            double integerMethod() {
+                if (foo) return Math.random()
+            }
+            def defMethod() {
+                if (foo) return Math.random()
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+        void testFieldsCastToLong() {
+            final SOURCE = '''
+                class MyClass {
+                    def a = (long) Math.random()
+                    def b = (Long) Math.random()
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, '(long) Math.random()', 'Casting the result of Math.random() to a long always results in 0',
+                    4, '(Long) Math.random()', 'Casting the result of Math.random() to a Long always results in 0')
+        }
+
+        void testLongFields() {
+            final SOURCE = '''
+                class MyClass {
+                    long c = Math.random()
+                    Long d = Math.random()
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, 'long c = Math.random()', 'Assigning the result of Math.random() to a long always results in 0',
+                    4, 'Long d = Math.random()', 'Assigning the result of Math.random() to a Long always results in 0')
+        }
+
+        void testFieldsAsLong() {
+            final SOURCE = '''
+                class MyClass {
+                    def e = (Math.random()) as long
+                    def f = (Math.random()) as Long
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, '(Math.random()) as long', 'Casting the result of Math.random() to a long always results in 0',
+                    4, '(Math.random()) as Long', 'Casting the result of Math.random() to a Long always results in 0')
+        }
+
+        void testLongReturningMethods() {
+            final SOURCE = '''
+                long longMethod() {
+                    if (foo) return Math.random()
+                }
+                Long longMethod2() {
+                    return ((foo) ?: Math.random())
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, 'Math.random()', 'Returning the result of Math.random() from a long-returning method always returns 0',
+                    6, 'Math.random()', 'Returning the result of Math.random() from a Long-returning method always returns 0')
+        }
+
+
+        void testFieldsCastToInt() {
+            final SOURCE = '''
+                class MyClass {
+                    def a = (int) Math.random()
+                    def b = (Integer) Math.random()
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, '(int) Math.random()', 'Casting the result of Math.random() to an int always results in 0',
+                    4, '(Integer) Math.random()', 'Casting the result of Math.random() to an Integer always results in 0')
+        }
+
+        void testIntFields() {
+            final SOURCE = '''
+                class MyClass {
+                    int c = Math.random()
+                    Integer d = Math.random()
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, 'int c = Math.random()', 'Assigning the result of Math.random() to an int always results in 0',
+                    4, 'Integer d = Math.random()', 'Assigning the result of Math.random() to an Integer always results in 0')
+        }
+
+        void testFieldsAsInt() {
+            final SOURCE = '''
+                class MyClass {
+                    def e = (Math.random()) as int
+                    def f = (Math.random()) as Integer
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, '(Math.random()) as int', 'Casting the result of Math.random() to an int always results in 0',
+                    4, '(Math.random()) as Integer', 'Casting the result of Math.random() to an Integer always results in 0')
+        }
+
+        void testIntReturningMethods() {
+            final SOURCE = '''
+                int intMethod() {
+                    if (foo) return Math.random()
+                }
+                Integer integerMethod() {
+                    if (foo) return Math.random()
+                }
+            '''
+            assertTwoViolations(SOURCE,
+                    3, 'Math.random()', 'Returning the result of Math.random() from an int-returning method always returns 0',
+                    6, 'Math.random()', 'Returning the result of Math.random() from an Integer-returning method always returns 0')
+        }
+
+    protected Rule createRule() {
+        new RandomDoubleCoercedToZeroRule()
+    }
+}
