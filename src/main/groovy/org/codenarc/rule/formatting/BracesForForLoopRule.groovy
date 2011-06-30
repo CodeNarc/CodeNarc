3a34985daed2409df1201f068360dbdda03cdb91
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-06-30 22:34:59 +0000

    braces for for-loop rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@790 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/formatting/BracesForForLoopRule.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.formatting
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.stmt.ForStatement
+
+/**
+ * Checks the location of the opening brace ({) for for loops. By default, requires them on a new line, but the
+ * sameLine property can be set to true to override this.
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 24 $ - $Date: 2009-01-31 13:47:09 +0100 (Sat, 31 Jan 2009) $
+ */
+class BracesForForLoopRule extends AbstractAstVisitorRule {
+    String name = 'BracesForForLoop'
+    int priority = 2
+    boolean sameLine = true
+    Class astVisitorClass = BracesForForLoopAstVisitor
+}
+
+class BracesForForLoopAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitForLoop(ForStatement node) {
+        if (rule.sameLine) {
+            if(!sourceLine(node)?.contains('{')) {
+                addViolation(node, 'Braces should start on the same line')
+            }
+        } else {
+            if(sourceLine(node)?.contains('{')) {
+                addViolation(node, 'Braces should start on a new line')
+            }
+        }
+        super.visitForLoop(node)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/formatting/BracesForForLoopRuleTest.groovy
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.formatting
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for BracesForForLoopRule
+ *
+ * @author Hamlet D'Arcy
+ * @version $Revision: 329 $ - $Date: 2010-04-29 04:20:25 +0200 (Thu, 29 Apr 2010) $
+ */
+class BracesForForLoopRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'BracesForForLoop'
+    }
+
+    void testNewLine() {
+        def testFile = this.getClass().getClassLoader().getResource('rule/BracesTestNewLine.txt')
+        final SOURCE = new File(testFile.toURI()).text
+        assertSingleViolation(SOURCE, 33, 'for (i in 0..3)', 'Braces should start on the same line')
+    }
+
+    void testNewLineOverride() {
+        def testFile = this.getClass().getClassLoader().getResource('rule/BracesTestNewLine.txt')
+        final SOURCE = new File(testFile.toURI()).text
+        rule.sameLine = false
+        assertNoViolations(SOURCE)
+    }
+
+    void testSameLine() {
+        def testFile = this.getClass().getClassLoader().getResource('rule/BracesTestSameLine.txt')
+        final SOURCE = new File(testFile.toURI()).text
+        assertNoViolations(SOURCE)
+    }
+
+    void testSameLineOverride() {
+        def testFile = this.getClass().getClassLoader().getResource('rule/BracesTestSameLine.txt')
+        final SOURCE = new File(testFile.toURI()).text
+        rule.sameLine = false
+        assertSingleViolation(SOURCE, 23, 'for (i in 0..3){', 'Braces should start on a new line')
+    }
+
+    protected Rule createRule() {
+        new BracesForForLoopRule()
+    }
+}
