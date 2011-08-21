75dfbeca232a5a93dd33968fa42e52cc99b02b62
Hamlet D'Arcy <hamletdrc@gmail.com>
2011-08-21 07:17:13 +0000

    long literal with small l rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@822 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/LongLiteralWithLowerCaseLRule.groovy
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * In Java and Groovy, you can specify long literals with the L or l character, for instance 55L or
+ * 24l. It is best practice to always use an uppercase L and never a lowercase l. This is
+ * because 11l rendered in some fonts may look like 111 instead of 11L.
+ *
+ * @author Hamlet D'Arcy
+ */
+class LongLiteralWithLowerCaseLRule extends AbstractAstVisitorRule {
+    String name = 'LongLiteralWithLowerCaseL'
+    int priority = 2
+    Class astVisitorClass = LongLiteralWithLowerCaseLAstVisitor
+}
+
+class LongLiteralWithLowerCaseLAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitConstantExpression(ConstantExpression expression) {
+
+        if (!isFirstVisit(expression)) {
+            return
+        }
+
+        if (expression.type.name == 'java.lang.Long') {
+            def line = getSourceCode().lines[expression.lineNumber - 1]
+            if (line?.length() <= expression.lastColumnNumber) {
+                def definition = line[expression.lastColumnNumber-2]
+                if (definition == 'l') {
+                    addViolation(expression, "The literal ${expression.value}l should be rewritten ${expression.value}L")
+                }
+            }
+        }
+        super.visitConstantExpression(expression)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/LongLiteralWithLowerCaseLRuleTest.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for LongLiteralWithLowerCaseLRule
+ *
+ * @author Hamlet D'Arcy
+ */
+class LongLiteralWithLowerCaseLRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'LongLiteralWithLowerCaseL'
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+        	def a = 55
+        	def b = 55L
+        	def x = 5
+        	def y = 5L
+        	def z = 5.0f
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testSingleViolation() {
+        final SOURCE = '''
+            def x = 1l
+        '''
+        assertSingleViolation(SOURCE, 2, 'def x = 1l', 'The literal 1l should be rewritten 1L')
+    }
+
+    void testLongerNUmber() {
+        final SOURCE = '''
+            def x = 222l
+        '''
+        assertSingleViolation(SOURCE, 2, 'def x = 222l', 'The literal 222l should be rewritten 222L')
+    }
+
+    protected Rule createRule() {
+        new LongLiteralWithLowerCaseLRule()
+    }
+}
