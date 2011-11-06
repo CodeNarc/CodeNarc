ad550168c378d7b794741f28bd9ca3cc71e13255
Chris Mair <chrismair@earthlink.net>
2011-11-06 22:01:22 +0000

    Rename HardcodedWindowsRootDirectory to HardCodedWindowsRootDirectory. #3433741.
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@885 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/HardCodedWindowsRootDirectoryRule.groovy
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2011 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.expr.*
+
+/**
+ * This rule find cases where a File object is constructed with a windows-based path. This is not portable, and using the File.listRoots() method is a better alternative. 
+ *
+ * @author Hamlet D'Arcy
+ */
+class HardCodedWindowsRootDirectoryRule extends AbstractAstVisitorRule {
+    String name = 'HardCodedWindowsRootDirectory'
+    int priority = 2
+    Class astVisitorClass = HardcodedWindowsRootDirectoryAstVisitor
+}
+
+class HardcodedWindowsRootDirectoryAstVisitor extends AbstractAstVisitor {
+    @Override
+    void visitConstructorCallExpression(ConstructorCallExpression call) {
+        if (isFirstVisit(call)) {
+
+            if (AstUtil.isConstructorCall(call, 'File') && call.arguments instanceof ArgumentListExpression) {
+                for (Expression exp : call.arguments.expressions) {
+                    addViolationForWindowsPath(exp)
+                }
+            }
+            super.visitConstructorCallExpression(call)
+        }
+    }
+
+    void addViolationForWindowsPath(Expression expression) {
+        if (expression instanceof ConstantExpression && expression.value instanceof String) {
+            if (expression.value ==~ /[a-zA-Z]:\\.*/) {
+                addViolation(expression, "The file location ${expression.value[0..2]} is not portable")
+            }
+        } else if (expression instanceof GStringExpression) {
+            if (expression.strings && expression.strings[0].value ==~ /[a-zA-Z]:\\.*/) {
+                addViolation(expression, "The file location ${expression.strings[0].value[0..2]} is not portable")
+            }
+        }
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/HardCodedWindowsRootDirectoryRuleTest.groovy
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for HardCodedWindowsRootDirectoryRule
+ *
+ * @author Hamlet D'Arcy
+ */
+class HardCodedWindowsRootDirectoryRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'HardCodedWindowsRootDirectory'
+    }
+
+    void testSuccessScenario() {
+        final SOURCE = '''
+           new File('cc:\\\\')
+           new File('a\\\\b\\\\c')
+           new File('/a/b')
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testCRoot() {
+        final SOURCE = '''
+           new File('c:\\\\')
+        '''
+        assertSingleViolation(SOURCE, 2, "new File('c:\\\\')", 'The file location c:\\ is not portable')
+    }
+
+    void testCRootTwoParm1() {
+        final SOURCE = '''
+           new File('c:\\\\', null)
+        '''
+        assertSingleViolation(SOURCE, 2, "new File('c:\\\\', null)", 'The file location c:\\ is not portable')
+    }
+
+    void testCRootTwoParm2() {
+        final SOURCE = '''
+           new File(null, 'c:\\\\')
+        '''
+        assertSingleViolation(SOURCE, 2, "new File(null, 'c:\\\\')", 'The file location c:\\ is not portable')
+    }
+
+    void testCRootAndDir() {
+        final SOURCE = '''
+           new File('c:\\\\dir')
+        '''
+        assertSingleViolation(SOURCE, 2, "new File('c:\\\\dir')", 'The file location c:\\ is not portable')
+    }
+
+    void testSingleViolation() {
+        final SOURCE = '''
+           new File('E:\\\\dir')
+        '''
+        assertSingleViolation(SOURCE, 2, "new File('E:\\\\dir')", 'The file location E:\\ is not portable')
+    }
+
+    void testGStringParameter() {
+        final SOURCE = '''
+           new File("E:\\\\dir\\\\$foo")
+        '''
+        assertSingleViolation(SOURCE, 2, 'new File("E:\\\\dir\\\\$foo")', 'The file location E:\\ is not portable')
+    }
+
+    protected Rule createRule() {
+        new HardCodedWindowsRootDirectoryRule()
+    }
+}
