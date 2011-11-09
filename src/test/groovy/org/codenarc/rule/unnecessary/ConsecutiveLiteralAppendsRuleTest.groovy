2eff9a75e823e6847148eca6706691b1f817b12a
Chris Mair <chrismair@earthlink.net>
2011-11-09 02:42:07 +0000

    Reorganize Basic ruleset
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@889 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

--- src/test/groovy/org/codenarc/rule/unnecessary/ConsecutiveLiteralAppendsRuleTest.groovy
+++ src/test/groovy/org/codenarc/rule/unnecessary/ConsecutiveLiteralAppendsRuleTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.codenarc.rule.basic
+package org.codenarc.rule.unnecessary
 
 import org.codenarc.rule.AbstractRuleTestCase
 import org.codenarc.rule.Rule
