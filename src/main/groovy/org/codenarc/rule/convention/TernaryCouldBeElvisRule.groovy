79557f35ae0a7f186d18ec3a18710251e92dd52d
Chris Mair <chrismair@earthlink.net>
2012-02-20 01:12:34 +0000

    New TerneryCouldBeElvis rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@930 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/convention/TernaryCouldBeElvisRule.groovy
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.convention
+
+import org.codehaus.groovy.ast.expr.ElvisOperatorExpression
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.expr.TernaryExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+
+/**
+ * Rule that checks for ternary expressions where the boolean and true expressions are the same.
+ * These can be simplified to an Elvis expression.
+ *
+ * @author Chris Mair
+ */
+class TernaryCouldBeElvisRule extends AbstractAstVisitorRule {
+    String name = 'TernaryCouldBeElvis'
+    int priority = 2
+    Class astVisitorClass = TernaryCouldBeElvisAstVisitor
+}
+
+class TernaryCouldBeElvisAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitTernaryExpression(TernaryExpression expression) {
+        if (isNotElvis(expression) && areTheSame(expression.booleanExpression.expression, expression.trueExpression)) {
+            def asElvis = "${expression.trueExpression.text} ?: ${expression.falseExpression.text}"
+            addViolation(expression, "${expression.text} can be simplified to $asElvis")
+        }
+        super.visitTernaryExpression(expression)
+    }
+
+    private boolean isNotElvis(TernaryExpression expression) {
+        return !(expression instanceof ElvisOperatorExpression)
+    }
+
+    private boolean areTheSame(Expression booleanExpression, Expression trueExpression) {
+        if (booleanExpression.class != trueExpression.class) {
+            return false
+        }
+        if (booleanExpression instanceof VariableExpression) {
+            return booleanExpression.name == trueExpression.name
+        }
+        if (booleanExpression instanceof MethodCallExpression) {
+            return booleanExpression.text == trueExpression.text
+        }
+        return false
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/convention/TernaryCouldBeElvisRuleTest.groovy
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.convention
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for TernaryCouldBeElvisRule
+ *
+ * @author Chris Mair
+ */
+class TernaryCouldBeElvisRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'TernaryCouldBeElvis'
+    }
+
+    void testElvis_NoViolation() {
+        final SOURCE = '''
+            x ?: 1
+            x ?: null
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testTernaryWithDifferentBooleanAndTrueVariableExpressions_NoViolation() {
+        final SOURCE = '''
+            (x == y) ? same : diff
+            (x) ? same : diff
+
+            x ? y : z
+            x ? 99 : 33
+            x ? x + 1 : x + 2
+            x ? 1 : 0
+            x ? !x : x
+            !x ? x : null
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testTernary_SimpleVariable_SameBooleanAndTrueExpression_Violation() {
+        final SOURCE = '''
+            x ? x : false
+        '''
+        assertSingleViolation(SOURCE, 2,'x ? x : false', 'x ?: false')
+    }
+
+    void testTernaryWithDifferentBooleanAndTrueMethodCalls_NoViolation() {
+        final SOURCE = '''
+            foo() ? bar() : 123
+            foo() ? foo(99) : 123
+            foo(x) ? foo() : 123
+            foo(1) ? foo(2) : 123
+            foo(1,2) ? foo(1) : 123
+            foo(1) ? !foo(1) : 123
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testTernary_MethodCall_SameBooleanAndTrueExpression_Violation() {
+        final SOURCE = '''
+            foo() ? foo() : bar()
+            foo(1) ? foo(1) : 123
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:2, sourceLineText:'foo() ? foo() : bar()', messageText:'foo() ?: this.bar()'],
+            [lineNumber:3, sourceLineText:'foo(1) ? foo(1) : 123', messageText:'foo(1) ?: 123'])
+    }
+
+    protected Rule createRule() {
+        new TernaryCouldBeElvisRule()
+    }
+}
