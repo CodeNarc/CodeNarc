8c08074dc996e1b9201908f19dd716307e00bad8
Chris Mair <chrismair@earthlink.net>
2012-02-26 13:43:19 +0000

    New BrokenNullCheck rule
    git-svn-id: https://codenarc.svn.sourceforge.net/svnroot/codenarc/trunk@936 58125288-d6d3-43b3-b1ee-2f4d32c6bf4a

---
+++ src/main/groovy/org/codenarc/rule/basic/BrokenNullCheckRule.groovy
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import static org.codenarc.util.AstUtil.*
+
+import org.codehaus.groovy.ast.expr.BinaryExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.ast.expr.PropertyExpression
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codehaus.groovy.ast.expr.NotExpression
+
+/**
+ * Looks for faulty checks for null in boolean conditions, e.g.
+ *      if (name != null || name.length > 0) { }
+ *      if (name != null || name.size() > 0) { }
+ *      if (string == null && string.equals("")) { }
+ *
+ * @author Chris Mair
+ */
+class BrokenNullCheckRule extends AbstractAstVisitorRule {
+    String name = 'BrokenNullCheck'
+    int priority = 2
+    Class astVisitorClass = BrokenNullCheckAstVisitor
+}
+
+class BrokenNullCheckAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitBinaryExpression(BinaryExpression expression) {
+        if (isFirstVisit(expression)) {
+            if (expression.leftExpression instanceof BinaryExpression) {
+                if (isBrokenNullCheck(expression, '||', '!=') || isBrokenNullCheck(expression, '&&', '==')) {
+                    addViolation(expression, "The expression ${expression.text} within class $currentClassName incorrectly checks for null, and can throw a NullPointerException.")
+                }
+            }
+        }
+        super.visitBinaryExpression(expression)
+    }
+
+    private boolean isBrokenNullCheck(BinaryExpression expression, String andOrOperator, String comparisonWithNullOperator) {
+        if (expression.operation.text == andOrOperator) {
+            def leftBinary = expression.leftExpression
+            def rightBinary = expression.rightExpression
+            if (isComparisonWithNull(leftBinary, comparisonWithNullOperator)) {
+                def varName = leftBinary.leftExpression.name
+                if (isExpressionAccessingMemberNamed(rightBinary, varName)) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private boolean isExpressionAccessingMemberNamed(Expression rightBinary, varName) {
+        return isPropertyAccessForName(rightBinary, varName) || isMethodCallOnName(rightBinary, varName)
+    }
+
+    private boolean isComparisonWithNull(BinaryExpression expression, String operation) {
+        return expression.leftExpression instanceof VariableExpression &&
+            expression.operation.text == operation &&
+            isNull(expression.rightExpression)
+    }
+
+    private boolean isPropertyAccessForName(Expression expression, String varName) {
+        boolean isDirectPropertyAccess =
+           expression instanceof PropertyExpression &&
+           expression.objectExpression instanceof VariableExpression &&
+           expression.objectExpression.name == varName
+        return isDirectPropertyAccess ||
+            isBinaryExpressionWithPropertyAccessForName(expression, varName) ||
+            isNotExpressionWithPropertyAccessForName(expression, varName)
+    }
+
+    private boolean isBinaryExpressionWithPropertyAccessForName(Expression expression, String varName) {
+         return expression instanceof BinaryExpression &&
+             isPropertyAccessForName(expression.leftExpression, varName)
+    }
+
+    private boolean isNotExpressionWithPropertyAccessForName(Expression expression, String varName) {
+         return expression instanceof NotExpression &&
+             isPropertyAccessForName(expression.expression, varName)
+    }
+
+    private boolean isMethodCallOnName(Expression expression, String varName) {
+        boolean isDirectMethodCall = expression instanceof MethodCallExpression &&
+            expression.objectExpression instanceof VariableExpression &&
+            expression.objectExpression.name == varName
+        return isDirectMethodCall ||
+            isBinaryExpressionWithMethodCallOnName(expression, varName) ||
+            isNotExpressionWithMethodCallOnName(expression, varName)
+    }
+
+    private boolean isBinaryExpressionWithMethodCallOnName(Expression expression, String varName) {
+         return expression instanceof BinaryExpression &&
+             isMethodCallOnName(expression.leftExpression, varName)
+    }
+
+    private boolean isNotExpressionWithMethodCallOnName(Expression expression, String varName) {
+         return expression instanceof NotExpression &&
+             isMethodCallOnName(expression.expression, varName)
+    }
+
+}
---
+++ src/test/groovy/org/codenarc/rule/basic/BrokenNullCheckRuleTest.groovy
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.basic
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+
+/**
+ * Tests for BrokenNullCheckRule
+ *
+ * @author Chris Mair
+ */
+class BrokenNullCheckRuleTest extends AbstractRuleTestCase {
+
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'BrokenNullCheck'
+    }
+
+    void testNoNullChecks_NoViolations() {
+        final SOURCE = '''
+            if (name == 'xxx' || name.length > 0) { }
+            (string != null && isReady()) ? 'yes' : 'no'
+            while (string != null) { }
+            def ok = string != null && x == 99
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testProperNullChecks_NoViolations() {
+        final SOURCE = '''
+            if (name == null || name.length < 1) { }
+            def notValid = name == null || name.length == 0
+            def notValidStr = (name == null || !name.size()) ? 'not valid' : 'valid'
+
+            if (string != null && string.length == 5) { }
+            if (string != null && string.equals("")) { }
+            while (nextRecord != null && nextRecord.getId()) { }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    void testBrokenNullChecks_PropertyAccess_Violations() {
+        final SOURCE = '''
+            if (name != null || name.length > 0) { }
+            if (name != null || name.length) { }
+            while (record == null && record.id < 10) { }
+            if (record == null && record.id && somethingElse()) { }
+            def isNotValid = record == null && record.id < 10
+            return record == null && !record.id
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:2, sourceLineText:'if (name != null || name.length > 0) { }', messageText:['name', 'None']],
+            [lineNumber:3, sourceLineText:'if (name != null || name.length) { }', messageText:'name'],
+            [lineNumber:4, sourceLineText:'while (record == null && record.id < 10) { }', messageText:'record'],
+            [lineNumber:5, sourceLineText:'if (record == null && record.id && somethingElse()) { }', messageText:'record'],
+            [lineNumber:6, sourceLineText:'def isNotValid = record == null && record.id < 10', messageText:'record'],
+            [lineNumber:7, sourceLineText:'return record == null && !record.id', messageText:'record'])
+    }
+
+    void testBrokenNullChecks_MethodCall_Violations() {
+        final SOURCE = '''
+            class MyClass {
+                def doStuff() {
+                    if (name != null || name.size() > 0) { }
+                    if (string == null && string.equals("")) { }
+                    def isValid = name != null || name.size() > 0
+                    return name != null || !name.size()
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:4, sourceLineText:'if (name != null || name.size() > 0) { }', messageText:['name', 'MyClass']],
+            [lineNumber:5, sourceLineText:'if (string == null && string.equals("")) { }', messageText:'string'],
+            [lineNumber:6, sourceLineText:'def isValid = name != null || name.size() > 0', messageText:'name'],
+            [lineNumber:7, sourceLineText:'return name != null || !name.size()', messageText:'name'])
+    }
+
+    protected Rule createRule() {
+        new BrokenNullCheckRule()
+    }
+}
