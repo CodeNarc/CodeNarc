021ce7fe449767fbc1c6fdc82b171b34b22114c0
Chris Mair <chrismair@earthlink.net>
2012-10-27 12:40:06 -0400

    New JUnitLostTest rule

---
+++ src/main/groovy/org/codenarc/rule/junit/JUnitLostTestRule.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.junit
+
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.rule.AbstractMethodVisitor
+import org.codehaus.groovy.ast.MethodNode
+import java.lang.reflect.Modifier
+import org.codenarc.util.AstUtil
+import org.codehaus.groovy.ast.ClassNode
+
+/**
+ * Rule that checks if a JUnit 4 test class contains public, instance, void, no-arg methods
+ * named test*() that are NOT annotated with @Test.
+ * <p/>
+ * This rule sets the default value of <code>applyToFilesMatching</code> to only match source code file
+ * paths ending in 'Test.groovy' or 'Tests.groovy'.
+ *
+ * @author Chris Mair
+  */
+class JUnitLostTestRule extends AbstractAstVisitorRule {
+
+    String name = 'JUnitLostTest'
+    int priority = 2
+    Class astVisitorClass = JUnitLostTestAstVisitor
+    String applyToClassNames = DEFAULT_TEST_CLASS_NAMES
+}
+
+class JUnitLostTestAstVisitor extends AbstractMethodVisitor {
+
+    @Override
+    void visitClass(ClassNode node) {
+        def imports = sourceCode.ast.imports + sourceCode.ast.starImports
+        def junit4TestClass = imports.find { importNode ->
+            def importName = importNode.className ?: importNode.packageName
+            importName.startsWith('org.junit.')
+        }
+
+        if (junit4TestClass) {
+            super.visitClass(node)
+        }
+    }
+
+    @Override
+    void visitMethod(MethodNode methodNode) {
+        if (Modifier.isPublic(methodNode.modifiers)
+            && (methodNode.isVoidMethod())
+            && (methodNode.parameters?.length == 0)
+            && (methodNode.name?.startsWith('test'))
+            && !(Modifier.isStatic(methodNode.modifiers))
+            && !AstUtil.hasAnnotation(methodNode, 'Test') ) {
+
+                addViolation(methodNode, "The method ${methodNode.name} is a public, instance, void, no-arg method  named test*() that is not annotated with @Test.")
+        }
+    }
+
+}
---
+++ src/test/groovy/org/codenarc/rule/junit/JUnitLostTestRuleTest.groovy
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.junit
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+import org.junit.Test
+
+/**
+ * Tests for JUnitLostTestRule
+ *
+ * @author Chris Mair
+  */
+class JUnitLostTestRuleTest extends AbstractRuleTestCase {
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'JUnitLostTest'
+    }
+
+    @Test
+    void testApplyTo_NonMatchingMethods_NoViolations() {
+        final SOURCE = '''
+            import org.junit.Test
+            class MyTestCase {
+                void doSomething() { }          // not named test*()
+                void testMe1(int count) { }     // not zero-arg
+                int testMe2() { }               // not void
+                private void testMe3() { }      // not public
+                static void testMe4() { }       // static
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testApplyTo_NotJUnit4TestClass_NoViolations() {
+        final SOURCE = '''
+            class MyTestCase {
+                void testMe() { }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testApplyTo_AnnotatedWithTest_NoViolations() {
+        final SOURCE = '''
+            import org.junit.Test
+            class MyTestCase {
+                @Test
+                void testMe() { }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testApplyTo_PublicVoidTestMethod_NoTestAnnotation_Violation() {
+        final SOURCE = '''
+            import org.junit.Test
+            class MyTestCase {
+                void testMe() { }
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, 'void testMe() { }')
+    }
+
+    @Test
+    void testApplyTo_ExplicitlyDeclaredPublicMethod_Violation() {
+        final SOURCE = '''
+            import org.junit.Test
+            class MyTestCase {
+                public void testMe() { }
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, 'public void testMe() { }')
+    }
+
+    @Test
+    void testApplyTo_OtherJUnit4Imports() {
+        final SOURCE = '''
+            import org.junit.After
+            class MyTestCase {
+                void testMe() { }
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, 'void testMe() { }')
+    }
+
+    @Test
+    void testApplyTo_StarImportOfJUnit4Package() {
+        final SOURCE = '''
+            import org.junit.*
+            class MyTestCase {
+                void testMe() { }
+            }
+        '''
+        assertSingleViolation(SOURCE, 4, 'void testMe() { }')
+    }
+
+    @Test
+    void testApplyTo_NonTestClass_NoViolations() {
+        final SOURCE = '''
+            import org.junit.*
+            class MyOtherClass {
+                void testMe() { }
+            }
+        '''
+        sourceCodePath = 'src/MyController.groovy'
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        new JUnitLostTestRule()
+    }
+}
