dfed4d3cbade28c25cca9a675b50c588b6032906
Chris Mair <chrismair@earthlink.net>
2012-12-01 19:56:14 -0500

    #3575859: New GrailsDuplicateMapping rule

---
+++ src/main/groovy/org/codenarc/rule/grails/GrailsDuplicateMappingRule.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.grails
+
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.rule.AbstractAstVisitor
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codehaus.groovy.ast.FieldNode
+
+/**
+ * Check for duplicate entry in a domain class mapping
+ *
+ * @author Chris Mair
+ */
+class GrailsDuplicateMappingRule extends AbstractAstVisitorRule {
+    String name = 'GrailsDuplicateMapping'
+    int priority = 2
+    Class astVisitorClass = GrailsDuplicateMappingAstVisitor
+    String applyToFilesMatching = GrailsUtil.DOMAIN_FILES
+}
+
+class GrailsDuplicateMappingAstVisitor extends AbstractAstVisitor {
+
+    private final Set<String> mappingNames = []
+    private boolean withinMapping
+
+    @Override
+    void visitField(FieldNode node) {
+        if (node.name == 'mapping') {
+            withinMapping = true
+        }
+        super.visitField(node)
+    }
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+        if (withinMapping && isFirstVisit(call)) {
+            def name = call.methodAsString
+            if (name in mappingNames) {
+                addViolation(call, "The mapping for $name in domain class $currentClassName has already been specified")
+            }
+            else {
+                mappingNames << name
+            }
+        }
+        super.visitMethodCallExpression(call)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/grails/GrailsDuplicateMappingRuleTest.groovy
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.grails
+
+import org.codenarc.rule.Rule
+import org.junit.Test
+import org.codenarc.rule.AbstractRuleTestCase
+import org.junit.Before
+
+/**
+ * Tests for GrailsDuplicateMappingRule
+ *
+ * @author Chris Mair
+ */
+class GrailsDuplicateMappingRuleTest extends AbstractRuleTestCase {
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'GrailsDuplicateMapping'
+    }
+
+    @Test
+    void testNoDuplicates_NoViolations() {
+        final SOURCE = '''
+            class Person {
+                String firstName
+                String lastName
+                static mapping = {
+                    table 'people'
+                    firstName column: 'First_Name'
+                    lastName column: 'Last_Name'
+                }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testEmptyMapping_NoViolations() {
+        final SOURCE = '''
+            class Person {
+                String firstName
+                static mapping = {
+                }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testCallsToOverloadedMethods_NoViolations() {
+        final SOURCE = '''
+            class Person {
+                void run() {
+                    doStuff()
+                    doStuff(99)
+                }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testDuplicateMappings_Violation() {
+        final SOURCE = '''
+            class Person {
+                String firstName
+                String lastName
+                static mapping = {
+                    table 'people'
+                    firstName column: 'First_Name'
+                    lastName column: 'Last_Name'
+                    firstName column: 'First_Name'
+                    table 'people2'
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:9, sourceLineText:"firstName column: 'First_Name'", messageText:'The mapping for firstName in domain class Person has already been specified'],
+            [lineNumber:10, sourceLineText:"table 'people2'", messageText:'The mapping for table in domain class Person has already been specified'])
+    }
+
+    @Test
+    void testDuplicateMappings_NestedColumnsClosure_Violation() {
+        final SOURCE = '''
+            class Person {
+                String firstName
+                String lastName
+                static mapping = {
+                    table 'people'
+                    columns {
+                        firstName column: 'First_Name'
+                        lastName column: 'Last_Name'
+                        firstName column: 'First_Name'
+                    }
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:10, sourceLineText:"firstName column: 'First_Name'", messageText:'The mapping for firstName in domain class Person has already been specified'])
+    }
+
+    @Test
+    void testNotDomainClass_DuplicateMappings_NoViolation() {
+        final SOURCE = '''
+            class Person {
+                static mapping = {
+                    table 'people'
+                    table 'people2'
+                }
+            }
+        '''
+        sourceCodePath = 'MyProject/other/Person.groovy'
+        assertNoViolations(SOURCE)
+    }
+
+    @Before
+    void setUp() {
+        sourceCodePath = 'MyProject/grails-app/domain/com/example/Person.groovy'
+    }
+
+    protected Rule createRule() {
+        new GrailsDuplicateMappingRule()
+    }
+}
