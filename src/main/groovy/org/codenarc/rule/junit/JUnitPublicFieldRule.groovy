bdc52674ae34bd537962c229dd14087345625377
Chris Mair <chrismair@earthlink.net>
2013-04-04 16:51:59 -0400

    #382. New JUnitPublicField rule

---
+++ src/main/groovy/org/codenarc/rule/junit/JUnitPublicFieldRule.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.junit
+
+import org.codehaus.groovy.ast.FieldNode
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.rule.AbstractFieldVisitor
+import org.codenarc.util.AstUtil
+
+/**
+ * Checks for public field on a JUnit test class. Ignores fields with the @Rule annotation.
+ * <p/>
+ * This rule sets the default value of <code>applyToFilesMatching</code> to only match source code file
+ * paths ending in 'Test.groovy' or 'Tests.groovy'.
+ *
+ * @author Chris Mair
+ */
+class JUnitPublicFieldRule extends AbstractAstVisitorRule {
+    String name = 'JUnitPublicField'
+    int priority = 2
+    Class astVisitorClass = JUnitPublicFieldAstVisitor
+    String applyToClassNames = DEFAULT_TEST_CLASS_NAMES
+}
+
+class JUnitPublicFieldAstVisitor extends AbstractFieldVisitor {
+
+    @Override
+    void visitField(FieldNode node) {
+        if (node.isPublic() && !AstUtil.hasAnnotation(node, 'Rule') ) {
+            addViolation(node, "The field $node.name is public. There is usually no reason to have a public field (even a constant) on a test class.")
+        }
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/junit/JUnitPublicFieldRuleTest.groovy
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.junit
+
+import org.codenarc.rule.Rule
+import org.junit.Test
+import org.codenarc.rule.AbstractRuleTestCase
+
+/**
+ * Tests for JUnitPublicFieldRule
+ *
+ * @author Chris Mair
+ */
+class JUnitPublicFieldRuleTest extends AbstractRuleTestCase {
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'JUnitPublicField'
+    }
+
+    @Test
+    void testTestClassWithNoPublicFields_NoViolations() {
+        final SOURCE = '''
+            import org.junit.Test
+            class MyTestCase {
+                private service
+                protected count = 99
+                def helper      // this is a property, not a public field
+                String name     // this is a property, not a public field
+
+                @Test
+                void testMe() { }
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testNonTestClassWithPublicFields_NoViolations() {
+        final SOURCE = '''
+            class MyClass {
+                public int count
+                public static final MAX_VALUE = 1000
+            }
+        '''
+        sourceCodePath = 'src/MyClass.groovy'
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testClassWithPublicFieldsAnnotatedWithRule_NoViolations() {
+        final SOURCE = '''
+            class MyTestCase {
+                @Rule public TestName testName = new TestName()
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testClassWithPublicFields_Violations() {
+        final SOURCE = '''
+            import org.junit.Test
+            class MyTestCase {
+                public int count
+                public static final MAX_VALUE = 1000
+                @Test
+                void testMe() { }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:4, sourceLineText:'public int count', messageText:'count'],
+            [lineNumber:5, sourceLineText:'public static final MAX_VALUE = 1000', messageText:'MAX_VALUE'])
+    }
+
+    protected Rule createRule() {
+        new JUnitPublicFieldRule()
+    }
+}
