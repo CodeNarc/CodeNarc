df006eeaaf354ac12630b6efc0eb5f9869cb9564
Chris Mair <chrismair@earthlink.net>
2013-07-06 09:38:03 -0400

    #421: SpaceAroundClosureArrow rule

---
+++ src/main/groovy/org/codenarc/rule/formatting/SpaceAroundClosureArrowRule.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.formatting
+
+import org.codehaus.groovy.ast.expr.ClosureExpression
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.rule.AbstractAstVisitor
+
+/**
+ * Checks that there is whitespace around the closure arrow (->) symbol
+ *
+ * @author Chris Mair
+ */
+class SpaceAroundClosureArrowRule extends AbstractAstVisitorRule {
+    String name = 'SpaceAroundClosureArrow'
+    int priority = 2
+    Class astVisitorClass = SpaceAroundClosureArrowAstVisitor
+}
+
+class SpaceAroundClosureArrowAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitClosureExpression(ClosureExpression expression) {
+        def line = sourceLine(expression) + ' '     // add trailing space in case -> is at end of line
+        if (line.contains('->') && !(line =~ /\s\-\>\s/)) {
+            addViolation(expression, "The closure arrow (->) within class $currentClassName is not surrounded by a space or whitespace")
+                //"The operator \"?\" within class $currentClassName is not followed by a space or whitespace")
+        }
+        super.visitClosureExpression(expression)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/formatting/SpaceAroundClosureArrowRuleTest.groovy
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.formatting
+
+import org.codenarc.rule.Rule
+import org.junit.Test
+import org.codenarc.rule.AbstractRuleTestCase
+
+/**
+ * Tests for SpaceAroundClosureArrowRule
+ *
+ * @author Chris Mair
+ */
+class SpaceAroundClosureArrowRuleTest extends AbstractRuleTestCase {
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'SpaceAroundClosureArrow'
+    }
+
+    @Test
+    void testNoViolations() {
+        final SOURCE = '''
+            def closure1 = { -> }
+            def closure2 = { count -> }
+            def closure3 = { count        ->                 }
+            def closure4 = { count\t->\t}
+            def closure5 = { count ->
+            }
+            def closure6 = { count, name
+             ->
+            }
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testKnownLimitation_ClosureErrorOnSeparateLine_NoViolations() {
+        final SOURCE = '''
+            def closure5 = { count, name
+             ->}
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testViolations() {
+        final SOURCE = '''
+            def closure1 = {->}
+            def closure2 = { ->}
+            def closure3 = {-> }
+            def closure4 = { count-> println 123 }
+            def closure5 = { count, name ->println 123 }
+            def closure6 = { ->println 123
+             }
+        '''
+        final MESSAGE = 'The closure arrow (->) within class None is not surrounded by a space or whitespace'
+        assertViolations(SOURCE,
+            [lineNumber:2, sourceLineText:'def closure1 = {->}', messageText:MESSAGE],
+            [lineNumber:3, sourceLineText:'def closure2 = { ->}', messageText:MESSAGE],
+            [lineNumber:4, sourceLineText:'def closure3 = {-> }', messageText:MESSAGE],
+            [lineNumber:5, sourceLineText:'def closure4 = { count-> println 123 }', messageText:MESSAGE],
+            [lineNumber:6, sourceLineText:'def closure5 = { count, name ->println 123 }', messageText:MESSAGE],
+            [lineNumber:7, sourceLineText:'def closure6 = { ->println 123', messageText:MESSAGE])
+    }
+
+    protected Rule createRule() {
+        new SpaceAroundClosureArrowRule()
+    }
+}
