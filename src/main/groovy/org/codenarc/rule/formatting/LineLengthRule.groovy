0a2b828ced6e93044a5a49e9b83699a933981fb9
Kyle Boon <kyle.f.boon@gmail.com>
2014-02-28 12:33:08 -0600

    Flags to ignore import and package line length
    The Google Java Style guide says that package and import statements
    should always be on a single line even if it is over the maximum (80 or
    100) character line length. This seems like a good rule to follow for
    groovy too.
    
    I added an ignorePackageStatement and ignoreImportStatement options to
    the LengthLengthRule. Both default to true.

--- src/main/groovy/org/codenarc/rule/formatting/LineLengthRule.groovy
+++ src/main/groovy/org/codenarc/rule/formatting/LineLengthRule.groovy
@@ -30,6 +30,8 @@ class LineLengthRule extends AbstractAstVisitorRule {
     String name = 'LineLength'
     int priority = 2
     int length = 120 // The default max line length. Can be overridden
+    boolean ignoreImportStatements = true // import statements can be longer than the max line legnth
+    boolean ignorePackageStatements = true // import statements can be longer than the max line legnth
 
     @Override
     void applyTo(SourceCode sourceCode, List violations) {
@@ -37,10 +39,37 @@ class LineLengthRule extends AbstractAstVisitorRule {
         int lineNumber = 0
         for (line in sourceCode.getLines()) {
             lineNumber++
-            if (line.length() > length) {
+            if (sourceViolatesLineLengthRule(line)) {
                 violations << createViolation(lineNumber, line, "The line exceeds $length characters. The line is ${line.length()} characters.")
             }
         }
     }
-}
 
+    private Boolean sourceViolatesLineLengthRule(String line) {
+        lineExceedsMaxLength(line) && (flagIfImport(line) || flagIfPackage(line) || flagIfRegularLine(line))
+    }
+
+    private Boolean flagIfImport(String line) {
+        (sourceLineIsImport(line) && !ignoreImportStatements)
+    }
+
+    private Boolean flagIfPackage(String line) {
+        (sourceLineIsPackage(line) && !ignorePackageStatements)
+    }
+
+    private Boolean flagIfRegularLine(String line) {
+        !sourceLineIsImport(line) && !sourceLineIsPackage(line)
+    }
+
+    private Boolean sourceLineIsImport(String line) {
+        line.trim().startsWith('import ')
+    }
+
+    private Boolean sourceLineIsPackage(String line) {
+        line.trim().startsWith('package ')
+    }
+
+    private Boolean lineExceedsMaxLength(String line) {
+        line.length() > length
+    }
+}
