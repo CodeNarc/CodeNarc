a0760418e5b07e2f8f992c3d95a4a155f02caadd
Joe Sondow <jsondow@netflix.com>
2014-03-15 20:42:34 -0700

    Removed blank lines preceding package statements
    This is in preparation for a commit of a BlankLineBeforePackage rule, including an assurance that existing unit tests will still pass when checking the CodeNarc source code for code style conventions.

--- src/main/groovy/org/codenarc/rule/concurrency/ThreadYieldRule.groovy
+++ src/main/groovy/org/codenarc/rule/concurrency/ThreadYieldRule.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.codenarc.rule.concurrency
 
 import org.codehaus.groovy.ast.expr.MethodCallExpression
