69fda466279686d1182d6d1b98257dbf94ff406c
Joe Sondow <jsondow@netflix.com>
2014-03-15 23:04:51 -0700

    Added a File Ends Without Newline rule
    Some reasons files should end with a newline: http://stackoverflow.com/questions/729692/why-should-files-end-with-a-newline

---
+++ src/test/groovy/org/codenarc/rule/formatting/FileEndsWithoutNewlineRuleTest.groovy
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.formatting
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+import org.junit.Test
+
+/**
+ * Tests for FileEndsWithoutNewlineRule
+ */
+class FileEndsWithoutNewlineRuleTest extends AbstractRuleTestCase {
+
+    def skipTestThatUnrelatedCodeHasNoViolations
+    def skipTestThatInvalidCodeHasNoViolations
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 3
+        assert rule.name == 'FileEndsWithoutNewline'
+    }
+
+    @Test
+    void testSuccessScenario() {
+        final SOURCE = '''
+            class MyClass {
+                    def go() { /* ... */ }
+            }
+        '''.stripIndent()
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testClassStartsWithDoubleBlankLines() {
+        final SOURCE = '''
+            class MyClass {
+                    void go() { /* ... */ }
+            }'''
+        assertSingleViolation(SOURCE, 3, '}', 'File null does not end with a newline')
+    }
+
+    protected Rule createRule() {
+        new FileEndsWithoutNewlineRule()
+    }
+}
