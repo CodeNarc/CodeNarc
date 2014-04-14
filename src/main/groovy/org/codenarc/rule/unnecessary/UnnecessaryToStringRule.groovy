8761ff3ee2d51316b9200fe452a731aa9ec751dd
Chris Mair <chrismair@earthlink.net>
2014-04-14 16:06:40 -0400

    #157: UnnecessaryToString rule

---
+++ src/main/groovy/org/codenarc/rule/unnecessary/UnnecessaryToStringRule.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2014 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codehaus.groovy.ast.FieldNode
+import org.codehaus.groovy.ast.expr.DeclarationExpression
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.util.AstUtil
+
+/**
+ * Checks for unnecessary calls to toString().
+ *
+ * @author Chris Mair
+ */
+class UnnecessaryToStringRule extends AbstractAstVisitorRule {
+
+    String name = 'UnnecessaryToString'
+    int priority = 2
+    Class astVisitorClass = UnnecessaryToStringAstVisitor
+}
+
+class UnnecessaryToStringAstVisitor extends AbstractAstVisitor {
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+        if (isFirstVisit(call) && AstUtil.isMethodCall(call, 'toString', 0) && isStringType(call.objectExpression)) {
+            addViolation(call, "Calling toString() on the String literal in class $currentClassName is unnecessary")
+        }
+        super.visitMethodCallExpression(call)
+    }
+
+    @Override
+    void visitField(FieldNode node) {
+        if (isStringType(node) && AstUtil.isMethodCall(node.initialExpression, 'toString', 0)) {
+            addViolation(node, "Calling toString() when assigning to String field \"${node.name}\" in class $currentClassName is unnecessary")
+        }
+        super.visitField(node)
+    }
+
+    @Override
+    void visitDeclarationExpression(DeclarationExpression expression) {
+        if (expression.leftExpression instanceof VariableExpression && isStringType(expression.leftExpression)) {
+            if (AstUtil.isMethodCall(expression.rightExpression, 'toString', 0)) {
+                def varName = expression.leftExpression.name
+                addViolation(expression, "Calling toString() when assigning to String variable \"$varName\" in class $currentClassName is unnecessary")
+            }
+        }
+        super.visitDeclarationExpression(expression)
+    }
+
+    private boolean isStringType(node) {
+        node.type.name in ['String', 'java.lang.String']
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/unnecessary/UnnecessaryToStringRuleTest.groovy
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.unnecessary
+
+import org.codenarc.rule.Rule
+import org.junit.Test
+import org.codenarc.rule.AbstractRuleTestCase
+
+/**
+ * Tests for UnnecessaryToStringRule
+ *
+ * @author Chris Mair
+ */
+class UnnecessaryToStringRuleTest extends AbstractRuleTestCase {
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'UnnecessaryToString'
+    }
+
+    @Test
+    void testNoViolations() {
+        final SOURCE = '''
+        	def name = nameNode.toString()
+        	def id = idNode.lastChild.toString()
+        	def code = "$id-1234".toString()     // GString
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testStringLiteral_ToString_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                def name = "Joe".toString()
+
+                void run() {
+                    def id = '123'.toString()
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:3, sourceLineText:'def name = "Joe".toString()', messageText:'Calling toString() on the String literal in class MyClass is unnecessary'],
+            [lineNumber:6, sourceLineText:"def id = '123'.toString()", messageText:'Calling toString() on the String literal in class MyClass is unnecessary'] )
+    }
+
+    @Test
+    void testAssignmentToStringField_ToString_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                String name = nameNode.toString()
+                String id = account.id.toString()
+                String code = account.getCode().toString()
+            }
+        '''
+        def message = 'Calling toString() when assigning to String field "%s" in class MyClass is unnecessary'
+        assertViolations(SOURCE,
+            [lineNumber:3, sourceLineText:'String name = nameNode.toString()', messageText:String.format(message, 'name')],
+            [lineNumber:4, sourceLineText:'String id = account.id.toString()', messageText:String.format(message, 'id')],
+            [lineNumber:5, sourceLineText:'String code = account.getCode().toString()', messageText:String.format(message, 'code')] )
+    }
+
+    @Test
+    void testAssignmentToStringVariable_ToString_Violation() {
+        final SOURCE = '''
+            String name = nameNode.toString()
+            String id = account.id.toString()
+            String code = account.getCode().toString()
+        '''
+        def message = 'Calling toString() when assigning to String variable "%s" in class None is unnecessary'
+        assertViolations(SOURCE,
+            [lineNumber:2, sourceLineText:'String name = nameNode.toString()', messageText:String.format(message, 'name')],
+            [lineNumber:3, sourceLineText:'String id = account.id.toString()', messageText:String.format(message, 'id')],
+            [lineNumber:4, sourceLineText:'String code = account.getCode().toString()', messageText:String.format(message, 'code')] )
+    }
+
+    protected Rule createRule() {
+        new UnnecessaryToStringRule()
+    }
+}
