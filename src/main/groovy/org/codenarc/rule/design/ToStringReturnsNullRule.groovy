9a8d43537dc277b21ed98c07d25683de6b5e9935
Chris Mair <chrismair@earthlink.net>
2014-04-16 10:02:43 -0400

    #422: New ToStringReturnsNull rule

---
+++ src/main/groovy/org/codenarc/rule/design/ToStringReturnsNullRule.groovy
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.stmt.ExpressionStatement
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.NullReturnTracker
+import org.codenarc.util.AstUtil
+
+/**
+ * Checks for toString() methods that return null.
+ *
+ * @author Chris Mair
+ */
+class ToStringReturnsNullRule extends AbstractAstVisitorRule {
+
+    String name = 'ToStringReturnsNull'
+    int priority = 2
+    Class astVisitorClass = ToStringReturnsNullAstVisitor
+}
+
+class ToStringReturnsNullAstVisitor extends AbstractAstVisitor {
+
+    private static final String ERROR_MESSAGE = 'The toString() method within class %s returns null'
+
+    @Override
+    protected void visitMethodEx(MethodNode node) {
+        if (AstUtil.isMethodNode(node, 'toString', 0)) {
+            def errorMessage = String.format(ERROR_MESSAGE, currentClassName)
+
+            // This will find all of the explicit return statements
+            node.code?.visit(new NullReturnTracker(parent:this, errorMessage:errorMessage))
+
+            checkForImplicitNullReturns(node, errorMessage)
+        }
+        super.visitMethodEx(node)
+    }
+
+    private void checkForImplicitNullReturns(MethodNode node, String errorMessage) {
+        def statements = node.code.statements
+        if (statements.empty) {
+            addViolation(node, errorMessage)
+            return
+        }
+
+        def lastStatement = statements[-1]
+        if (lastStatement instanceof ExpressionStatement && AstUtil.isNull(lastStatement.expression)) {
+            addViolation(lastStatement, errorMessage)
+        }
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/design/ToStringReturnsNullRuleTest.groovy
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codenarc.rule.Rule
+import org.junit.Test
+import org.codenarc.rule.AbstractRuleTestCase
+
+/**
+ * Tests for ToStringReturnsNullRule
+ *
+ * @author Chris Mair
+ */
+class ToStringReturnsNullRuleTest extends AbstractRuleTestCase {
+
+    private static final String ERROR_MESSAGE = 'The toString() method within class MyClass returns null'
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'ToStringReturnsNull'
+    }
+
+    @Test
+    void testToString_ReturnsNonNull_NoViolation() {
+        final SOURCE = '''
+        	class MyClass {
+        	    String toString() {
+        	        return 'MyClass'
+        	    }
+        	}
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testToStringWithParameters_NoViolation() {
+        final SOURCE = '''
+        	class MyClass {
+        	    String toString(int count) { return null }
+        	    void toString(String name) { return null }
+        	    String toString(String name, int count) { return null }
+        	}
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void testSingleReturn_ReturnsNull_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                String toString() {
+                    return null
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:4, sourceLineText:'return null', messageText:ERROR_MESSAGE])
+    }
+
+    @Test
+    void testNoReturnKeyword_Null_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                String toString() {
+                    calculateStuff()
+                    null
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:5, sourceLineText:'null', messageText: ERROR_MESSAGE])
+    }
+
+    @Test
+    void testEmptyMethod_ImplicitlyReturnsNull_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                String toString() {
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:3, sourceLineText:'String toString()', messageText: ERROR_MESSAGE])
+    }
+
+    @Test
+    void testMultipleReturns_MayReturnNull_Violation() {
+        final SOURCE = '''
+            class MyClass {
+                String toString() {
+                    if (foo()) {
+                        return 'MyClass'
+                    } else {
+                        return null
+                    }
+                }
+            }
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:7, sourceLineText:'return null', messageText:ERROR_MESSAGE])
+    }
+
+    protected Rule createRule() {
+        new ToStringReturnsNullRule()
+    }
+}
