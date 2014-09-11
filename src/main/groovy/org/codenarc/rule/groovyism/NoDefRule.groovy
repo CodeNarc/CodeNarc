e47b6608476903e9c2e41ff48dc70b19e0b085c2
Dominik Przybysz <dpr@touk.pl>
2014-09-11 10:38:32 +0200

    Fix tests

--- src/main/groovy/org/codenarc/rule/groovyism/NoDefRule.groovy
+++ src/main/groovy/org/codenarc/rule/groovyism/NoDefRule.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codenarc.rule.groovyism
 
 import org.codenarc.rule.AbstractRule
@@ -15,7 +30,7 @@ import java.util.regex.Pattern
  * @author Dominik Przybysz
  */
 class NoDefRule extends AbstractRule {
-    String name = "No def"
+    String name = 'NoDef'
     int priority = 3
     protected static final String MESSAGE = 'def should not be used'
     String excludePattern
@@ -25,13 +40,13 @@ class NoDefRule extends AbstractRule {
         Pattern excludeFilter = excludePattern ? ~/.*def\s+$excludePattern.*/ : null
         sourceCode.lines.eachWithIndex {
             String line, int idx ->
-                if (line.contains("def ") && (!excludeFilter || !(line ==~ excludeFilter))){
-                    Violation violation = new Violation();
-                    violation.setRule(this);
-                    violation.setLineNumber(idx + 1);
-                    violation.setSourceLine(line.trim());
-                    violation.setMessage(MESSAGE);
-                    violations.add(violation);
+                if (line.contains('def ') && (!excludeFilter || !(line ==~ excludeFilter))) {
+                    Violation violation = new Violation()
+                    violation.setRule(this)
+                    violation.setLineNumber(idx + 1)
+                    violation.setSourceLine(line.trim())
+                    violation.setMessage(MESSAGE)
+                    violations.add(violation)
                 }
         }
     }
--- src/test/groovy/org/codenarc/rule/groovyism/NoDefRuleTest.groovy
+++ src/test/groovy/org/codenarc/rule/groovyism/NoDefRuleTest.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codenarc.rule.groovyism
 
 import org.codenarc.rule.AbstractRuleTestCase
@@ -7,34 +22,34 @@ import org.junit.Test
 /**
  * @author Dominik Przybysz
  */
-public class NoDefRuleTest extends AbstractRuleTestCase {
+class NoDefRuleTest extends AbstractRuleTestCase {
 
     @Test
     void testSuccessScenario() {
-        final SOURCE = '''
+        final SOURCE = '''\
             List l = [1, 2, 3, 4]
             l.flatten()
-        '''
+        '''.stripMargin()
         assertNoViolations(SOURCE)
     }
 
     @Test
     void testSingleViolation() {
-        final SOURCE = '''
+        final SOURCE = '''\
             def l = [1, 2, 3, 4]
             l.flatten()
-        '''
+        '''.stripMargin()
         assertSingleViolation SOURCE, 1, 'def l = [1, 2, 3, 4]', NoDefRule.MESSAGE
     }
 
     @Test
     void testTwoViolation() {
-        final SOURCE = '''
+        final SOURCE = '''\
             def test(def l){
                 int k = 3
                 def i = 5
             }
-        '''
+        '''.stripMargin()
         assertTwoViolations (SOURCE,
                 1, 'def test(def l){', NoDefRule.MESSAGE,
                 3, 'def i = 5', NoDefRule.MESSAGE)
@@ -43,13 +58,13 @@ public class NoDefRuleTest extends AbstractRuleTestCase {
     @Test
     void testExcludesNoViolation() {
         rule.excludePattern = /((setup|cleanup)(|Spec)|"[^"].*")\(\)/ //spock methods
-        final SOURCE = '''
+        final SOURCE = '''\
             def setup(){}
             def setupSpec(){}
             def cleanup(){}
             def cleanupSpec(){}
             def "should send"(){}
-        '''
+        '''.stripMargin()
         assertNoViolations(SOURCE)
     }
 
