f51c182c8f3ef4cd0763c91869b7df7b83a5aa20
Chris Mair <cmair42@gmail.com>
2015-06-17 10:00:24 -0400

    #110: New AssignmentToStaticFieldFromInstanceMethod rule

---
+++ src/main/groovy/org/codenarc/rule/design/AssignmentToStaticFieldFromInstanceMethodRule.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codehaus.groovy.ast.ClassNode
+import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.expr.BinaryExpression
+import org.codehaus.groovy.ast.expr.DeclarationExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codenarc.rule.AbstractAstVisitorRule
+import org.codenarc.rule.AbstractAstVisitor
+
+/**
+ * Checks for assignment to a static field from an instance method.
+ *
+ * @author Chris Mair
+ */
+class AssignmentToStaticFieldFromInstanceMethodRule extends AbstractAstVisitorRule {
+
+    String name = 'AssignmentToStaticFieldFromInstanceMethod'
+    int priority = 2
+    Class astVisitorClass = AssignmentToStaticFieldFromInstanceMethodAstVisitor
+}
+
+class AssignmentToStaticFieldFromInstanceMethodAstVisitor extends AbstractAstVisitor {
+
+    private Collection<String> fieldNames
+    private String withinInstanceMethodName = null
+    private final Set<String> localVariableNames = []
+
+    @Override
+    protected void visitClassEx(ClassNode node) {
+        def staticFields = node.getFields().findAll { f -> f.isStatic() }
+        this.fieldNames = staticFields.name
+        super.visitClassEx(node)
+    }
+
+    @Override
+    protected void visitMethodEx(MethodNode node) {
+        withinInstanceMethodName = node.isStatic() ? null : node.name
+        super.visitMethodEx(node)
+    }
+
+    @Override
+    protected void visitMethodComplete(MethodNode node) {
+        withinInstanceMethodName = null
+        localVariableNames.clear()
+        super.visitMethodComplete(node)
+    }
+
+    @Override
+    void visitDeclarationExpression(DeclarationExpression expression) {
+        if (expression.leftExpression instanceof VariableExpression) {
+            localVariableNames.add(expression.leftExpression.name)
+        }
+        super.visitDeclarationExpression(expression)
+    }
+
+    @Override
+    void visitBinaryExpression(BinaryExpression expression) {
+        boolean isAssignment = expression.operation.text == '='
+        if (isAssignment && withinInstanceMethodName && expression.leftExpression instanceof VariableExpression) {
+            String name = expression.leftExpression.name
+            if (fieldNames.contains(name) && !localVariableNames.contains(name)) {
+                addViolation(expression, "The instance method $withinInstanceMethodName in class $currentClassName contains an assignment to static field $name")
+            }
+        }
+        super.visitBinaryExpression(expression)
+    }
+}
---
+++ src/test/groovy/org/codenarc/rule/design/AssignmentToStaticFieldFromInstanceMethodRuleTest.groovy
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.design
+
+import org.codenarc.rule.Rule
+import org.junit.Test
+import org.codenarc.rule.AbstractRuleTestCase
+
+/**
+ * Tests for AssignmentToStaticFieldFromInstanceMethodRule
+ *
+ * @author Chris Mair
+ */
+class AssignmentToStaticFieldFromInstanceMethodRuleTest extends AbstractRuleTestCase {
+
+    @Test
+    void testRuleProperties() {
+        assert rule.priority == 2
+        assert rule.name == 'AssignmentToStaticFieldFromInstanceMethod'
+    }
+
+    @Test
+    void test_AssignmentToInstanceFields_NoViolations() {
+        final SOURCE = '''
+        	class MyClass {
+        	    private field1
+        	    protected String field2 = 'abc'
+        	    public int field3 = 123
+        	    public final field4 = 456
+        	    String property1 = 'abc'
+
+        	    private void doStuff() {
+        	        field1 = new Object()
+        	        field2 = 'xxx'
+        	        field3 = 999
+        	        property1 = 'xxx'
+        	    }
+        	}
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void test_AssignmentToStaticFields_FromStaticMethods_NoViolations() {
+        final SOURCE = '''
+        	class MyClass {
+        	    private static field1
+        	    protected static String field2 = 'abc'
+        	    public static int field3 = 123
+        	    public static final field4 = 456
+        	    static String property1 = 'abc'
+
+        	    private static void doStuff() {
+        	        field1 = new Object()
+        	        field2 = 'xxx'
+        	        field3 = 999
+        	        property1 = 'xxx'
+        	    }
+        	}
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void test_AssignmentToStaticField_FromInstanceMethod_Violations() {
+        final SOURCE = '''
+        	class MyClass {
+        	    private static field1
+        	    protected static String field2 = 'abc'
+        	    public static int field3 = 123
+        	    static String property1 = 'abc'
+
+        	    private void doStuff() {
+        	        field1 = new Object()
+        	        field2 = 'xxx'
+        	        field3 = 999
+        	        property1 = 'xxx'
+        	    }
+        	}
+        '''
+        assertViolations(SOURCE,
+            [lineNumber:9, sourceLineText:'field1 = new Object()', messageText:'The instance method doStuff in class MyClass contains an assignment to static field field1'],
+            [lineNumber:10, sourceLineText:"field2 = 'xxx'", messageText:'The instance method doStuff in class MyClass contains an assignment to static field field2'],
+            [lineNumber:11, sourceLineText:'field3 = 999', messageText:'The instance method doStuff in class MyClass contains an assignment to static field field3'],
+            [lineNumber:12, sourceLineText:"property1 = 'xxx'", messageText:'The instance method doStuff in class MyClass contains an assignment to static field property1'])
+    }
+
+    @Test
+    void test_ReferencesToStaticField_FromInstanceMethods_NoViolations() {
+        final SOURCE = '''
+        	class MyClass {
+        	    private static field1
+        	    protected static String field2 = 'abc'
+        	    public static int field3 = 123
+        	    static String property1 = 'abc'
+
+        	    private void doStuff() {
+        	        println field1 + 'suffix'
+        	        def isReady = field2 > 'xxx'
+        	        println field3 == 999
+        	        property1 < 'xxx'
+        	    }
+        	}
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void test_AssignmentToLocalVariableWithSameNameAsStaticField_NoViolations() {
+        final SOURCE = '''
+        	class MyClass {
+        	    private static final TEXT
+        	    public static String NAME
+
+        	    private void doStuff() {
+        	        final TEXT = "new"
+        	        def NAME
+        	        NAME = 'joe'
+        	    }
+        	}
+        '''
+        assertNoViolations(SOURCE)
+    }
+
+    protected Rule createRule() {
+        new AssignmentToStaticFieldFromInstanceMethodRule()
+    }
+}
