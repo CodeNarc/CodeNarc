33a659121cd23d849df2fd12fb41c6f3a5d271ed
Chris Mair <cmair42@gmail.com>
2015-06-27 19:41:18 -0400

    AbstractHtmlReportWriter

---
+++ src/main/groovy/org/codenarc/report/AbstractHtmlReportWriter.groovy
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2015 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.report
+
+import groovy.xml.StreamingMarkupBuilder
+import org.codenarc.AnalysisContext
+import org.codenarc.results.Results
+import org.codenarc.rule.Rule
+import org.codenarc.util.io.ClassPathResource
+
+/**
+ * Abstract superclass for HTML ReportWriter classes.
+ *
+ * @author Chris Mair
+ */
+@SuppressWarnings('DuplicateMapLiteral')
+abstract class AbstractHtmlReportWriter extends AbstractReportWriter {
+
+    protected static final MAX_SOURCE_LINE_LENGTH = 70
+    protected static final SOURCE_LINE_LAST_SEGMENT_LENGTH = 12
+
+    String title
+    boolean includeRuleDescriptions = true
+    int maxPriority = 3
+
+    /**
+     * @return the path and filename for the CSS file
+     */
+    abstract protected String getCssFile()
+
+    abstract protected buildBodySection(AnalysisContext analysisContext, results)
+
+    /**
+     * Write out a report to the specified Writer for the analysis results
+     * @param analysisContext - the AnalysisContext containing the analysis configuration information
+     * @param results - the analysis results
+     */
+    void writeReport(Writer writer, AnalysisContext analysisContext, Results results) {
+        assert analysisContext
+        assert results
+
+        initializeResourceBundle()
+        def builder = new StreamingMarkupBuilder()
+        def html = builder.bind {
+            html {
+                out << buildHeaderSection()
+                out << buildBodySection(analysisContext, results)
+            }
+        }
+        writer << html
+    }
+
+    //--------------------------------------------------------------------------
+    // Internal Helper Methods
+    //--------------------------------------------------------------------------
+
+    protected buildCSS() {
+        return {
+            def cssInputStream = ClassPathResource.getInputStream(getCssFile())
+            assert cssInputStream, "CSS File [$getCssFile()] not found"
+            def css = cssInputStream.text
+            style(type: 'text/css') {
+                unescaped << css
+            }
+        }
+    }
+
+    protected buildHeaderSection() {
+        return {
+            head {
+                title(buildTitle())
+                out << buildCSS()
+            }
+        }
+    }
+
+    protected buildReportMetadata() {
+        return {
+            div(class: 'metadata') {
+                table {
+                    tr {
+                        td(class: 'em', getResourceBundleString('htmlReport.reportTitle.title'))
+                        td title
+                    }
+                    tr {
+                        td(class: 'em', getResourceBundleString('htmlReport.reportTimestamp.label'))
+                        td getFormattedTimestamp()
+                    }
+                    tr {
+                        td(class: 'em', getResourceBundleString('htmlReport.reportVersion.label'))
+                        td { a("CodeNarc v${getCodeNarcVersion()}", href:CODENARC_URL) }
+                    }
+                }
+            }
+        }
+    }
+
+    protected buildLogo() {
+        return {
+            img(class: 'logo', src: 'http://codenarc.sourceforge.net/images/codenarc-logo.png', alt: 'CodeNarc', align: 'right')
+       }
+    }
+
+    protected buildRuleDescriptions(AnalysisContext analysisContext) {
+        def sortedRules = getSortedRules(analysisContext)
+
+        return {
+            div(class: 'summary') {
+                h2(getResourceBundleString('htmlReport.ruleDescriptions.title'))
+                table(border:'1') {
+                    tr(class:'tableHeader') {
+                        th('#', class:'ruleDescriptions')
+                        th(getResourceBundleString('htmlReport.ruleDescriptions.ruleNameHeading'), class:'ruleDescriptions')
+                        th(getResourceBundleString('htmlReport.ruleDescriptions.descriptionHeading'), class:'ruleDescriptions')
+                    }
+
+                    sortedRules.eachWithIndex { Rule rule, index ->
+                        def ruleName = rule.name
+                        def priority = rule.priority
+                        tr(class:'ruleDescriptions') {
+                            td {
+                                a(name:ruleName) { }
+                                span(index + 1,class:'ruleIndex')    
+                            }
+                            td(ruleName, class:"ruleName priority${priority}")
+                            td { unescaped << getHtmlDescriptionForRule(rule) }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Format and trim the source line. If the whole line fits, then include the whole line (trimmed).
+     * Otherwise, remove characters from the middle to truncate to the max length.
+     * @param sourceLine - the source line to format
+     * @param startColumn - the starting column index; used to truncate the line if it's too long; defaults to 0
+     * @return the formatted and trimmed source line
+     */
+    protected String formatSourceLine(String sourceLine, int startColumn=0) {
+        def source = sourceLine ? sourceLine.trim() : null
+        if (source && source.size() > MAX_SOURCE_LINE_LENGTH) {
+            source = startColumn ? sourceLine[startColumn..-1] : sourceLine.trim()
+            def lengthOfFirstSegment = MAX_SOURCE_LINE_LENGTH - SOURCE_LINE_LAST_SEGMENT_LENGTH - 2
+            def firstSegment = source[0..lengthOfFirstSegment - 1]
+            def lastSegment = source[-SOURCE_LINE_LAST_SEGMENT_LENGTH..-1]
+            source = firstSegment + '..' + lastSegment
+        }
+        return source
+    }
+
+    /**
+     * Return true if the Results represents a directory that contains at least one file with one
+     * or more violations.
+     * @param results - the Results
+     */
+    protected boolean isDirectoryContainingFilesWithViolations(Results results) {
+        return !results.isFile() && results.getNumberOfFilesWithViolations(maxPriority, false)
+    }
+
+    /**
+     * Return true if the Results represents a directory that contains at least one file
+     * @param results - the Results
+     */
+    protected boolean isDirectoryContainingFiles(Results results) {
+        return !results.isFile() && results.getTotalNumberOfFiles(false)
+    }
+
+    protected String buildTitle() {
+        getResourceBundleString('htmlReport.titlePrefix')  + (title ? ": $title" : '')
+    }
+
+}
--- src/main/groovy/org/codenarc/report/HtmlReportWriter.groovy
+++ src/main/groovy/org/codenarc/report/HtmlReportWriter.groovy
@@ -15,12 +15,8 @@
  */
 package org.codenarc.report
 
-import groovy.xml.StreamingMarkupBuilder
 import org.codenarc.AnalysisContext
-import org.codenarc.results.Results
-import org.codenarc.rule.Rule
 import org.codenarc.rule.Violation
-import org.codenarc.util.io.ClassPathResource
 
 /**
  * ReportWriter that generates an HTML report.
@@ -43,70 +39,29 @@ import org.codenarc.util.io.ClassPathResource
  *
  * @author Chris Mair
  */
-@SuppressWarnings(['DuplicateMapLiteral', 'UnnecessaryReturnKeyword', 'FactoryMethodName'])
-class HtmlReportWriter extends AbstractReportWriter {
+@SuppressWarnings('DuplicateMapLiteral')
+class HtmlReportWriter extends AbstractHtmlReportWriter {
 
     public static final DEFAULT_OUTPUT_FILE = 'CodeNarcReport.html'
     private static final CSS_FILE = 'codenarc-htmlreport.css'
     private static final ROOT_PACKAGE_NAME = '<Root>'
-    private static final MAX_SOURCE_LINE_LENGTH = 70
-    private static final SOURCE_LINE_LAST_SEGMENT_LENGTH = 12
 
-    String title
     String defaultOutputFile = DEFAULT_OUTPUT_FILE
     boolean includeSummaryByPackage = true
-    boolean includeRuleDescriptions = true
-    int maxPriority = 3
-
-    /**
-     * Write out a report to the specified Writer for the analysis results
-     * @param analysisContext - the AnalysisContext containing the analysis configuration information
-     * @param results - the analysis results
-     */
-    void writeReport(Writer writer, AnalysisContext analysisContext, Results results) {
-        assert analysisContext
-        assert results
-
-        initializeResourceBundle()
-        def builder = new StreamingMarkupBuilder()
-        def html = builder.bind {
-            html {
-                out << buildHeaderSection()
-                out << buildBodySection(analysisContext, results)
-            }
-        }
-        writer << html
-    }
 
     String toString() {
         "HtmlReportWriter[outputFile=$outputFile, title=$title]"
+    }
+
+    protected String getCssFile() {
+        return CSS_FILE
     }
 
     //--------------------------------------------------------------------------
     // Internal Helper Methods
     //--------------------------------------------------------------------------
 
-    private buildCSS() {
-        return {
-            def cssInputStream = ClassPathResource.getInputStream(CSS_FILE)
-            assert cssInputStream, "CSS File [$CSS_FILE] not found"
-            def css = cssInputStream.text
-            style(type: 'text/css') {
-                unescaped << css
-            }
-        }
-    }
-
-    private buildHeaderSection() {
-        return {
-            head {
-                title(buildTitle())
-                out << buildCSS()
-            }
-        }
-    }
-
-    private buildBodySection(AnalysisContext analysisContext, results) {
+    protected buildBodySection(AnalysisContext analysisContext, results) {
         return {
             body {
                 // TODO: copy the image or inline it in the css
@@ -121,33 +76,6 @@ class HtmlReportWriter extends AbstractReportWriter {
                 }
             }
         }
-    }
-
-    private buildReportMetadata() {
-        return {
-            div(class: 'metadata') {
-                table {
-                    tr {
-                        td(class: 'em', getResourceBundleString('htmlReport.reportTitle.title'))
-                        td title
-                    }
-                    tr {
-                        td(class: 'em', getResourceBundleString('htmlReport.reportTimestamp.label'))
-                        td getFormattedTimestamp()
-                    }
-                    tr {
-                        td(class: 'em', getResourceBundleString('htmlReport.reportVersion.label'))
-                        td { a("CodeNarc v${getCodeNarcVersion()}", href:CODENARC_URL) }
-                    }
-                }
-            }
-        }
-    }
-
-    private buildLogo() {
-        return {
-            img(class: 'logo', src: 'http://codenarc.sourceforge.net/images/codenarc-logo.png', alt: 'CodeNarc', align: 'right')
-       }
     }
 
     private buildSummaryByPackage(results) {
@@ -289,76 +217,6 @@ class HtmlReportWriter extends AbstractReportWriter {
                 }
             }
         }
-    }
-
-    private buildRuleDescriptions(AnalysisContext analysisContext) {
-        def sortedRules = getSortedRules(analysisContext)
-
-        return {
-            div(class: 'summary') {
-                h2(getResourceBundleString('htmlReport.ruleDescriptions.title'))
-                table(border:'1') {
-                    tr(class:'tableHeader') {
-                        th('#', class:'ruleDescriptions')
-                        th(getResourceBundleString('htmlReport.ruleDescriptions.ruleNameHeading'), class:'ruleDescriptions')
-                        th(getResourceBundleString('htmlReport.ruleDescriptions.descriptionHeading'), class:'ruleDescriptions')
-                    }
-
-                    sortedRules.eachWithIndex { Rule rule, index ->
-                        def ruleName = rule.name
-                        def priority = rule.priority
-                        tr(class:'ruleDescriptions') {
-                            td {
-                                a(name:ruleName) { }
-                                span(index + 1,class:'ruleIndex')    
-                            }
-                            td(ruleName, class:"ruleName priority${priority}")
-                            td { unescaped << getHtmlDescriptionForRule(rule) }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Format and trim the source line. If the whole line fits, then include the whole line (trimmed).
-     * Otherwise, remove characters from the middle to truncate to the max length.
-     * @param sourceLine - the source line to format
-     * @param startColumn - the starting column index; used to truncate the line if it's too long; defaults to 0
-     * @return the formatted and trimmed source line
-     */
-    protected String formatSourceLine(String sourceLine, int startColumn=0) {
-        def source = sourceLine ? sourceLine.trim() : null
-        if (source && source.size() > MAX_SOURCE_LINE_LENGTH) {
-            source = startColumn ? sourceLine[startColumn..-1] : sourceLine.trim()
-            def lengthOfFirstSegment = MAX_SOURCE_LINE_LENGTH - SOURCE_LINE_LAST_SEGMENT_LENGTH - 2
-            def firstSegment = source[0..lengthOfFirstSegment - 1]
-            def lastSegment = source[-SOURCE_LINE_LAST_SEGMENT_LENGTH..-1]
-            source = firstSegment + '..' + lastSegment
-        }
-        return source
-    }
-
-    /**
-     * Return true if the Results represents a directory that contains at least one file with one
-     * or more violations.
-     * @param results - the Results
-     */
-    protected boolean isDirectoryContainingFilesWithViolations(Results results) {
-        return !results.isFile() && results.getNumberOfFilesWithViolations(maxPriority, false)
-    }
-
-    /**
-     * Return true if the Results represents a directory that contains at least one file
-     * @param results - the Results
-     */
-    protected boolean isDirectoryContainingFiles(Results results) {
-        return !results.isFile() && results.getTotalNumberOfFiles(false)
-    }
-
-    private String buildTitle() {
-        getResourceBundleString('htmlReport.titlePrefix')  + (title ? ": $title" : '')
     }
 
 }
