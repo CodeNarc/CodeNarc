d5ee6416ff240c1985f82284777f33591cc26d7e
Sargis Harutyunyan <sargis.harutyunyan@webbfontaine.com>
2017-06-01 00:22:07 +0200

    Using AST rule to check def usages

--- src/main/groovy/org/codenarc/rule/convention/NoDefRule.groovy
+++ src/main/groovy/org/codenarc/rule/convention/NoDefRule.groovy
@@ -15,9 +15,13 @@
  */
 package org.codenarc.rule.convention
 
-import org.codenarc.rule.AbstractRule
-import org.codenarc.rule.Violation
-import org.codenarc.source.SourceCode
+import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.Parameter
+import org.codehaus.groovy.ast.Variable
+import org.codehaus.groovy.ast.expr.DeclarationExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
 
 import java.util.regex.Pattern
 
@@ -29,20 +33,83 @@ import java.util.regex.Pattern
  *
  * @author Dominik Przybysz
  */
-class NoDefRule extends AbstractRule {
+class NoDefRule extends AbstractAstVisitorRule {
+
+    public static final String MESSAGE = 'def for declaration should not be used'
+    public static final String MESSAGE_DEF_RETURN = 'def for method return type should not be used'
+    public static final String MESSAGE_DEF_PARAMETER = 'def for method parameter type should not be used'
+
     String name = 'NoDef'
     int priority = 3
-    protected static final String MESSAGE = 'def should not be used'
-    String excludeRegex
+    Pattern excludeFilter
+
+    Class astVisitorClass = NoDefAstVisitor
+
+    void setExcludeRegex(String excludeRegex) {
+        this.excludeFilter = excludeRegex ? ~/$excludeRegex/ : null
+    }
+}
+
+class NoDefAstVisitor extends AbstractAstVisitor {
+
+    private static final Pattern CONTAINS_WHITESPACE_PATTERN = ~/.*\s+.*/
 
     @Override
-    void applyTo(SourceCode sourceCode, List<Violation> violations) {
-        Pattern excludeFilter = excludeRegex ? ~/.*def\s+$excludeRegex.*/ : null
-        sourceCode.lines.eachWithIndex {
-            String line, int idx ->
-                if (line.contains('def ') && (!excludeFilter || !(line ==~ excludeFilter))) {
-                    violations << createViolation(idx + 1, line.trim(), MESSAGE)
-                }
+    void visitDeclarationExpression(DeclarationExpression expression) {
+        VariableExpression variableExpression = expression.getVariableExpression()
+
+        if (dynamicVariable(variableExpression)) {
+            addViolation(variableExpression, NoDefRule.MESSAGE)
         }
+        super.visitDeclarationExpression(expression)
+    }
+
+    @Override
+    protected void visitConstructorOrMethod(MethodNode node, boolean isConstructor) {
+        if (methodExcluded(node)) {
+            return
+        }
+
+        if (node.isDynamicReturnType()) {
+            addViolation(node, NoDefRule.MESSAGE_DEF_RETURN)
+        }
+
+        visitParameters(node.getParameters())
+
+        super.visitConstructorOrMethod(node, isConstructor)
+    }
+
+    private void visitParameters(Parameter[] parameters) {
+        if (parameters) {
+            parameters.each { Parameter param ->
+                if (dynamicVariable(param)) {
+                    addViolation(param, NoDefRule.MESSAGE_DEF_PARAMETER)
+                }
+            }
+        }
+    }
+
+    private Pattern getMatcherPattern() {
+        return rule.excludeFilter
+    }
+
+    private boolean methodExcluded(MethodNode node) {
+        return node.isDynamicReturnType() && methodExcluded(matcherPattern, node.name)
+    }
+
+    private static boolean methodExcluded(Pattern pattern, String text) {
+        matches(CONTAINS_WHITESPACE_PATTERN, text) || matches(pattern, "${text}()")
+    }
+
+    private boolean dynamicVariable(Variable variableExpression) {
+        return variableExpression.isDynamicTyped() && variableNotExcluded(matcherPattern, variableExpression.name)
+    }
+
+    private static boolean variableNotExcluded(Pattern pattern, String text) {
+        !matches(pattern, text)
+    }
+
+    private static boolean matches(Pattern pattern, String text) {
+        pattern && pattern.matcher(text).matches()
     }
 }
