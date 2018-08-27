1bbb3edfe685ff5e497b149670f198157cb59495
tarkil <phantomquest@gmail.com>
2018-08-27 23:32:51 +0200

    Fix minor typo

--- src/main/groovy/org/codenarc/rule/braces/ClassEndsWithBlankLineBeforeClosingBraceRequiredRule.groovy
+++ src/main/groovy/org/codenarc/rule/braces/ClassEndsWithBlankLineBeforeClosingBraceRequiredRule.groovy
@@ -24,13 +24,13 @@ import org.codenarc.rule.Violation
 import org.codenarc.util.AstUtil
 
 /**
- * Check the location of the closing brace of a class. By default, enforce that there must be a blank line before
+ * Check the location of the closing brace of a class. By default, it enforces that there must be a blank line before
  * the closing class brace, except if the class is empty and is written in a single line. A blank line is defined as
  * any line that does not contain any visible characters.
  * This rule can be configured with the following properties:
  * <ul>
  *  <li><i>singleLineClassesAllowed</i>: a boolean property to forbid single line classes.
- *  If it is false single line classes are considered a violation. Default value is true</li>
+ *  If it is false, then single line classes are considered a violation. Default value is true</li>
  *  <li><i>blankLineBeforeClosingBrace</i>: a boolean property to define if there may be a blank line before the closing
  *  class brace. If it is false, the last line before the brace must not be blank. Otherwise, it must be blank. Default
  *  value is true</li>
