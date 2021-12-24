f22283e8cd8b1ff25ae96c90b9224f4eb4cabf74
Chris Mair <cmair42@gmail.com>
2021-12-24 09:46:19 -0500

    #650: Add test for public method with annotations. Revisit after upgrade to Groovy 3.x.

--- src/test/groovy/org/codenarc/rule/unnecessary/UnnecessaryPublicModifierRuleTest.groovy
+++ src/test/groovy/org/codenarc/rule/unnecessary/UnnecessaryPublicModifierRuleTest.groovy
@@ -16,23 +16,29 @@
 package org.codenarc.rule.unnecessary
 
 import org.codenarc.rule.AbstractRuleTestCase
+import org.junit.Ignore
 import org.junit.Test
 
 /**
  * Tests for UnnecessaryPublicModifierRule
  *
  * @author Hamlet D'Arcy
+ * @author Chris Mair
   */
 class UnnecessaryPublicModifierRuleTest extends AbstractRuleTestCase<UnnecessaryPublicModifierRule> {
 
+    private static final String MESSAGE_CLASSES = 'The public keyword is unnecessary for classes'
+    private static final String MESSAGE_METHODS = 'The public keyword is unnecessary for methods'
+    private static final String MESSAGE_CONSTRUCTORS = 'The public keyword is unnecessary for constructors'
+
     @Test
-    void testRuleProperties() {
+    void test_RuleProperties() {
         assert rule.priority == 3
         assert rule.name == 'UnnecessaryPublicModifier'
     }
 
     @Test
-    void testSuccessScenario() {
+    void test_NoViolations() {
         final SOURCE = '''
             class MyClass {
                 void "my public Method"() {}
@@ -48,85 +54,87 @@ class UnnecessaryPublicModifierRuleTest extends AbstractRuleTestCase<Unnecessary
     }
 
     @Test
-    void testClass0() {
-        final SOURCE = '''public class MyClass {
+    void test_Class() {
+        final SOURCE = '''public class MyClass1 {
+            }
+            
+            public class MyClass2 {
             }
         '''
-        assertSingleViolation(SOURCE, 1, 'public class MyClass', 'The public keyword is unnecessary for classes')
+        assertViolations(SOURCE,
+                [line:1, source:'public class MyClass1', message: MESSAGE_CLASSES],
+                [line:4, source:'public class MyClass2', message: MESSAGE_CLASSES])
     }
 
     @Test
-    void testClass() {
-        final SOURCE = '''
-            public class MyClass {
-            }
-        '''
-        assertSingleViolation(SOURCE, 2, 'public class MyClass', 'The public keyword is unnecessary for classes')
-    }
-
-    @Test
-    void testClassSplit() {
-        final SOURCE = '''
-            public
-            class MyClass {
-            }
-        '''
-        assertSingleViolation(SOURCE, 2, 'public', 'The public keyword is unnecessary for classes')
-    }
-
-    @Test
-    void testClassSplit2() {
+    void test_Class_Split() {
         final SOURCE = '''public
-            class MyClass
+            class MyClass1
             {
             }
+            
+            public
+            class MyClass2 {
+            }
         '''
-        assertSingleViolation(SOURCE, 1, 'public', 'The public keyword is unnecessary for classes')
+        assertViolations(SOURCE,
+                [line:1, source:'public', message: MESSAGE_CLASSES],
+                [line:6, source:'public', message: MESSAGE_CLASSES])
     }
 
     @Test
-    void testMethodSplit() {
+    void test_Constructor() {
+        final SOURCE = '''
+            class MyClass {
+                public MyClass() {}
+            }
+        '''
+        assertSingleViolation(SOURCE, 3, 'public MyClass() {}', MESSAGE_CONSTRUCTORS)
+    }
+
+    @Test
+    void test_Constructor_PublicGeneric() {
+        final SOURCE = '''
+             class MyClass<T, K> {
+                public MyClass(T t, K k) { }
+            }
+        '''
+        assertSingleViolation(SOURCE, 3, 'public MyClass(T t, K k) { }', MESSAGE_CONSTRUCTORS)
+    }
+
+    @Test
+    void test_Class_PublicGeneric() {
+        final SOURCE = '''
+            public class MyClass<T> {
+                T someProperty
+            }
+        '''
+        assertSingleViolation(SOURCE, 2, 'public class MyClass<T>', MESSAGE_CLASSES)
+    }
+
+    @Test
+    void test_Method_Split() {
         final SOURCE = '''
             class MyClass {
                 public
                 void myMethod() {}
             }
         '''
-        assertSingleViolation(SOURCE, 3, 'public', 'The public keyword is unnecessary for methods')
+        assertSingleViolation(SOURCE, 3, 'public', MESSAGE_METHODS)
     }
 
     @Test
-    void testMethod() {
+    void test_Method() {
         final SOURCE = '''
             class MyClass {
                 public void myMethod() {}
             }
         '''
-        assertSingleViolation(SOURCE, 3, 'public void myMethod()', 'The public keyword is unnecessary for methods')
+        assertSingleViolation(SOURCE, 3, 'public void myMethod()', MESSAGE_METHODS)
     }
 
     @Test
-    void testConstructor() {
-        final SOURCE = '''
-            class MyClass {
-                public MyClass() {}
-            }
-        '''
-        assertSingleViolation(SOURCE, 3, 'public MyClass() {}', 'The public keyword is unnecessary for constructors')
-    }
-
-    @Test
-    void testPublicGenericClass() {
-        final SOURCE = '''
-            public class MyClass<T> {
-                T someProperty
-            }
-        '''
-        assertSingleViolation(SOURCE, 2, 'public class MyClass<T>', 'The public keyword is unnecessary for classes')
-    }
-
-    @Test
-    void testPublicGenericMethodWithReturnGenericType() {
+    void test_Method_PublicGeneric_ReturnGenericType() {
         final SOURCE = '''
              class MyClass {
                 public <T> T myMethod() { }
@@ -136,7 +144,7 @@ class UnnecessaryPublicModifierRuleTest extends AbstractRuleTestCase<Unnecessary
     }
 
     @Test
-    void testPublicGenericMethodWithVoidReturnType() {
+    void test_Method_PublicGeneric_VoidReturnType() {
         final SOURCE = '''
              class MyClass {
                 public <T> void myMethod(T t) { }
@@ -145,14 +153,22 @@ class UnnecessaryPublicModifierRuleTest extends AbstractRuleTestCase<Unnecessary
         assertNoViolations(SOURCE)
     }
 
+    @Ignore
     @Test
-    void testPublicGenericConstructor() {
+    void test_Method_AnnotationWithParametersContainingPublic() {
         final SOURCE = '''
-             class MyClass<T, K> {
-                public MyClass(T t, K k) { }
+            class FakeTest {
+                @ParameterizedTest
+                @ValueSource(strings = [
+                        'some public value',
+                        'some other value',
+                ])
+                void testNothing(final String value) {
+                    assertFalse(value.isEmpty())
+                }
             }
         '''
-        assertSingleViolation(SOURCE, 3, 'public MyClass(T t, K k) { }', 'The public keyword is unnecessary for constructors')
+        assertNoViolations(SOURCE)
     }
 
     @Override
