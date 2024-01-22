9068efe06451023937aeda7f8194d7ec7b4ec73e
danielzaenker <44169349+danielzaenker@users.noreply.github.com>
2024-01-22 14:10:09 +0100

    add CpsCallFromNonCpsMethodRule
    on-behalf-of: @e-solutions-GmbH <info@esolutions.de>

--- docs/codenarc-rules-jenkins.md
+++ docs/codenarc-rules-jenkins.md
@@ -4,3 +4,45 @@ title: CodeNarc - Jenkins Rules
 ---  
 
 # Jenkins Rules  ("*rulesets/jenkins.xml*")
+
+## CpsCallFromNonCpsMethod Rule
+
+*Since CodeNarc 3.5.0*
+
+CPS transformed methods may not be called from non CPS transformed methods in Jenkins.
+Every analyzed Method is assumed to be CPS transformed, except it is annotated with `com.cloudbees.groovy.cps.NonCPS`, is a constructor or a library method that is not configured in `cpsPackages`.
+Pipeline steps are assumed to be CPS transformed except for 'echo', 'properties' and 'getContext'.
+See also the Jenkins docs on [Pipeline CPS Method Mismatches](https://www.jenkins.io/doc/book/pipeline/cps-method-mismatches/).
+
+| Property              | Description                                       | Default Value |
+|-----------------------|---------------------------------------------------|---------------|
+| cpsScriptVariableName | Jenkins pipeline script variable name             | 'script'      |
+| cpsPackages           | Package names of used Jenkins pipeline libraries  | []            |
+
+Known limitations:
+
+* Methods are only checked for their name, parameters are ignored. Therefore false positives are possible if two overloaded methods are both CPS and non CPS transformed respectively.
+* Because of their dynamic nature, pipeline steps can only be recognized if they are called on a predefined script variable (default is `script`).
+* Method calls on dynamic types (Object) can't be resolved.
+
+Examples:
+
+```
+class SomeClass() {
+    public int value = cpsMethod() // Violation
+    SomeClass() {}
+}
+
+@NonCPS
+void nonCpsMethod() {}
+
+void cpsMethod() {}
+
+@NonCPS
+void someMethod() {
+    nonCpsMethod() // OK
+    new SomeClass() // OK
+    cpsMethod() // Violation
+    script.sh('echo hello') // Violation
+}
+```
---
+++ src/main/groovy/org/codenarc/rule/jenkins/CpsCallFromNonCpsMethodRule.groovy
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codenarc.rule.jenkins
+
+import org.codehaus.groovy.ast.*
+import org.codehaus.groovy.ast.expr.MethodCall
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codehaus.groovy.ast.expr.StaticMethodCallExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.control.Phases
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * CPS transformed methods may not be called from non CPS transformed methods in Jenkins
+ *
+ * @author Daniel Zänker
+ */
+class CpsCallFromNonCpsMethodRule extends AbstractAstVisitorRule {
+
+    String name = 'CpsCallFromNonCpsMethod'
+    int priority = 1
+    Class astVisitorClass = CpsCallFromNonCpsMethodAstVisitor
+    int compilerPhase = Phases.SEMANTIC_ANALYSIS
+    String cpsScriptVariableName = 'script'
+    List<String> cpsPackages = []
+
+}
+
+class CpsCallFromNonCpsMethodAstVisitor extends AbstractAstVisitor {
+
+    private final static List<String> PIPELINE_STEP_CPS_EXCEPTIONS = ['echo', 'properties', 'getContext']
+
+    private MethodNode currentMethod
+    private boolean cpsContext = false
+
+    @Override
+    protected void visitMethodEx(MethodNode methodNode) {
+        cpsContext = JenkinsUtil.isCpsMethod(methodNode, false)
+        currentMethod = methodNode
+    }
+
+    @Override
+    protected void visitMethodComplete(MethodNode node) {
+        currentMethod = null
+        cpsContext = false
+    }
+
+    @Override
+    void visitConstructor(ConstructorNode constructorNode) {
+        cpsContext = JenkinsUtil.isCpsMethod(constructorNode, true)
+        currentMethod = constructorNode
+        for (Parameter parameter : constructorNode.parameters) {
+            if (parameter.hasInitialExpression()) {
+                parameter.initialExpression.visit(this)
+            }
+        }
+        super.visitConstructor(constructorNode)
+        currentMethod = null
+        cpsContext = false
+    }
+    private ClassNode getReceiverClass(MethodCall call, boolean implicit) {
+        if (implicit) {
+            return currentClassNode
+        }
+        return JenkinsUtil.getReceiverType(call)
+    }
+
+    private boolean isCallOnScriptVariable(MethodCall call) {
+        if (call.receiver instanceof VariableExpression) {
+            VariableExpression variableExpression = (VariableExpression) call.receiver
+            return variableExpression.name == rule.cpsScriptVariableName
+        }
+        return false
+    }
+
+    private static boolean isCpsPipelineStep(MethodCall call) {
+        return call.methodAsString !in PIPELINE_STEP_CPS_EXCEPTIONS
+    }
+
+    private boolean isInCpsPackage(MethodNode methodNode) {
+        if (methodNode.declaringClass.packageName) {
+            return rule.cpsPackages.any { String packageName -> methodNode.declaringClass.packageName.startsWith(packageName) }
+        }
+        // assume always CPS for default package (e.g. Jenkinsfile)
+        return true
+    }
+
+    @Override
+    void visitStaticMethodCallExpression(StaticMethodCallExpression call) {
+        checkMethodCall(call, !call.receiver)
+        super.visitStaticMethodCallExpression(call)
+    }
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+        checkMethodCall(call, call.implicitThis)
+        super.visitMethodCallExpression(call)
+    }
+
+    private <T extends ASTNode & MethodCall> void checkMethodCall(T call, boolean implicit) {
+        if (cpsContext) {
+            return
+        }
+
+        if (isCallOnScriptVariable(call) && isCpsPipelineStep(call)) {
+            if (currentMethod) {
+                addViolation(call, "The method ${call.methodAsString} is a CPS transformed pipeline step and may not be called from non-CPS transformed method ${currentClassNode.name}.${currentMethod.name}")
+            } else {
+                addViolation(call, "The method ${call.methodAsString} is a CPS transformed pipeline step and may not be called from non-CPS transformed initialization code in ${currentClassNode.name}")
+            }
+            return
+        }
+
+        ClassNode receiverClass = getReceiverClass(call, implicit)
+        if (receiverClass == ClassHelper.DYNAMIC_TYPE) {
+            // can't check methods for dynamic type
+            return
+        }
+
+        String methodName = call.methodAsString
+        if (receiverClass.getMethods(methodName).any { JenkinsUtil.isCpsMethod(it, it instanceof ConstructorNode) && isInCpsPackage(it) }) {
+            if (currentMethod) {
+                addViolation(call, "The method ${receiverClass.name}.${methodName} is CPS transformed and may not be called from non-CPS transformed method ${currentClassNode.name}.${currentMethod.name}")
+            } else {
+                addViolation(call, "The method ${receiverClass.name}.$methodName is CPS transformed and may not be called from non-CPS transformed initialization code in ${currentClassNode.name}")
+            }
+        }
+    }
+}
